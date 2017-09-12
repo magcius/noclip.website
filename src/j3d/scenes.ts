bd1024d57025f24f2a8a9c26465481aed2961734
Jasper St. Pierre <jstpierre@mecheye.net>
2017-09-11 23:37:37 -0700

    Add partially working J3D

---
+++ src/j3d/scenes.ts
@@ -0,0 +1,14 @@
+
+import { SceneDesc } from 'render';
+import { SceneGroup } from '../viewer';
+
+const name = "J3D Models";
+const sceneDescs:SceneDesc[] = [
+    { name: "Faceship", filename: "faceship.bmd" },
+].map((entry):SceneDesc => {
+    const path = `data/j3d/${entry.filename}`;
+    const name = entry.name || entry.filename;
+    return new SceneDesc(name, path);
+});
+
+export const sceneGroup:SceneGroup = { name, sceneDescs };
