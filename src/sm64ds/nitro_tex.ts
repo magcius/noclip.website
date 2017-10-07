1e2cfd98f23aef2cd61434a8b6ead5afcaa60cb0
Jasper St. Pierre <jstpierre@mecheye.net>
2017-10-07 15:11:07 -0700

    fix texture

--- src/sm64ds/nitro_tex.ts
+++ src/sm64ds/nitro_tex.ts
@@ -11,71 +11,57 @@ export enum Format {
     Tex_Direct =     0x07,
 };
 
-function color(a, r, g, b) {
-    return (a << 24) | (r << 16) | (g << 8) | b;
+function expand3to8(n:number):number {
+    return (n << (8-3)) | (n << (8-6)) | (n >>> (9-8));
 }
 
-function rgb5(pixel:number, alpha:number) {
-    var r, g, b;
-    r = (pixel & 0x7c00) >>> 10;
-    r = (r << (8-5)) | (r >>> (10-8));
-
-    g = (pixel & 0x3e0) >>> 5;
-    g = (g << (8-5)) | (g >>> (10-8));
-
-    b = pixel & 0x1f;
-    b = (b << (8-5)) | (b >>> (10-8));
-
-    return color(alpha, r, g, b);
+function expand5to8(n:number):number {
+    return (n << (8-5)) | (n >>> (10-8));
 }
 
-function writeColor(pixels:Uint8Array, dstPixel:number, pixel:number) {
-    var dstOffs = dstPixel * 4;
-    var a = ((pixel >>> 24) & 0xFF);
-    var r = ((pixel >>> 16) & 0xFF);
-    var g = ((pixel >>>  8) & 0xFF);
-    var b = ((pixel >>>  0) & 0xFF);
-    pixels[dstOffs++] = b;
-    pixels[dstOffs++] = g;
-    pixels[dstOffs++] = r;
-    pixels[dstOffs++] = a;
+function s3tcblend(a:number, b:number):number {
+    // return (a*3 + b*5) / 8;
+    return (((a << 1) + a) + ((b << 2) + b)) >>> 3;
 }
 
 function readTexture_A3I5(width:number, height:number, texData:ArrayBuffer, palData:ArrayBuffer) {
-    var pixels = new Uint8Array(width * height * 4);
-    var texView = new DataView(texData);
-    var palView = new DataView(palData);
-    var srcOffs = 0;
-    for (var y = 0; y < height; y++) {
-        for (var x = 0; x < width; x++) {
-            var texBlock = texView.getUint8(srcOffs++);
-            var palIdx = (texBlock & 0x1F) << 1;
-            var color = rgb5(palView.getUint16(palIdx, true), 0);
-            var alpha = texBlock >>> 5;
-            alpha = (alpha << (8-3)) | (alpha >>> (6-8));
-            var pixel = alpha << 24 | color;
-            var dstPixel = (y * width) + x;
-            writeColor(pixels, dstPixel, pixel);
-            texBlock >>= 2;
+    const pixels = new Uint8Array(width * height * 4);
+    const texView = new DataView(texData);
+    const palView = new DataView(palData);
+    let srcOffs = 0;
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const texBlock = texView.getUint8(srcOffs++);
+            const palIdx = (texBlock & 0x1F) << 1;
+            const alpha = texBlock >>> 5;
+            const p = palView.getUint16(palIdx, true);
+            const dstOffs = 4 * ((y * width) + x);
+            pixels[dstOffs+0] = expand5to8(p & 0x1F);
+            pixels[dstOffs+1] = expand5to8((p >>> 5) & 0x1F);
+            pixels[dstOffs+2] = expand5to8((p >>> 10) & 0x1F);
+            pixels[dstOffs+3] = expand3to8(alpha);
         }
     }
     return pixels;
 }
 
 function readTexture_Palette16(width:number, height:number, texData:ArrayBuffer, palData:ArrayBuffer, color0:boolean) {
-    var pixels = new Uint8Array(width * height * 4);
-    var texView = new DataView(texData);
-    var palView = new DataView(palData);
-    var srcOffs = 0;
-    for (var y = 0; y < height; y++) {
-        for (var xx = 0; xx < width; xx += 4) {
-            var texBlock = texView.getUint16(srcOffs, true);
+    const pixels = new Uint8Array(width * height * 4);
+    const texView = new DataView(texData);
+    const palView = new DataView(palData);
+    let srcOffs = 0;
+    for (let y = 0; y < height; y++) {
+        for (let xx = 0; xx < width; xx += 4) {
+            let texBlock = texView.getUint16(srcOffs, true);
             srcOffs += 2;
             for (var x = 0; x < 4; x++) {
-                var palIdx = texBlock & 0x0F;
-                var pixel = rgb5(palView.getUint16(palIdx, true), color0 ? 0x00 : 0xFF);
-                var dstPixel = (y * width) + xx + x;
-                writeColor(pixels, dstPixel, pixel);
+                const palIdx = texBlock & 0x0F;
+                const p = palView.getUint16(palIdx, true);
+                const dstOffs = 4 * ((y * width) + xx + x);
+                pixels[dstOffs+0] = expand5to8(p & 0x1F);
+                pixels[dstOffs+1] = expand5to8((p >>> 5) & 0x1F);
+                pixels[dstOffs+2] = expand5to8((p >>> 10) & 0x1F);
+                pixels[dstOffs+3] = color0 ? 0x00 : 0xFF;
                 texBlock >>= 4;
             }
         }
@@ -84,88 +70,89 @@ function readTexture_Palette16(width:number, height:number, texData:ArrayBuffer,
 }
 
 function readTexture_CMPR_4x4(width:number, height:number, texData:ArrayBuffer, palData:ArrayBuffer) {
-    function mix(p1, p2) {
-        var a = (((p1 >>> 24) & 0xFF) + ((p2 >>> 24) & 0xFF)) >>> 1;
-        var r = (((p1 >>> 16) & 0xFF) + ((p2 >>> 16) & 0xFF)) >>> 1;
-        var g = (((p1 >>>  8) & 0xFF) + ((p2 >>>  8) & 0xFF)) >>> 1;
-        var b = (((p1 >>>  0) & 0xFF) + ((p2 >>>  0) & 0xFF)) >>> 1;
-        return color(a, r, g, b);
-    }
+    function buildColorTable(palBlock) {
+        const palMode = palBlock >> 14;
+        const palOffs = (palBlock & 0x3FFF) << 2;
 
-    function mixs3(p1, p2) {
-        // p1*(5/8) + p2*(3/8)
-        var a1 = ((p1 >>> 24) & 0xFF);
-        var r1 = ((p1 >>> 16) & 0xFF);
-        var g1 = ((p1 >>>  8) & 0xFF);
-        var b1 = ((p1 >>>  0) & 0xFF);
+        const colorTable = new Uint8Array(16);
 
-        var a2 = ((p2 >>> 24) & 0xFF);
-        var r2 = ((p2 >>> 16) & 0xFF);
-        var g2 = ((p2 >>>  8) & 0xFF);
-        var b2 = ((p2 >>>  0) & 0xFF);
+        const p0 = palView.getUint16(palOffs + 0x00, true);
+        colorTable[0] = expand5to8(p0 & 0x1F);
+        colorTable[1] = expand5to8((p0 >>> 5) & 0x1F);
+        colorTable[2] = expand5to8((p0 >>> 10) & 0x1F);
+        colorTable[3] = 0xFF;
 
-        var a = ((a1 >>> 1) + (a1 >>> 4)) + ((a2 >>> 1) - (a2 >>> 4));
-        var r = ((r1 >>> 1) + (r1 >>> 4)) + ((r2 >>> 1) - (r2 >>> 4));
-        var g = ((g1 >>> 1) + (g1 >>> 4)) + ((g2 >>> 1) - (g2 >>> 4));
-        var b = ((b1 >>> 1) + (b1 >>> 4)) + ((b2 >>> 1) - (b2 >>> 4));
-        return color(a, r, g, b);
-    }
-
-    function buildPalette(palBlock) {
-        function getPal(offs) {
-            if (offs >= palView.byteLength)
-                return 0xFF000000;
-            return rgb5(palView.getUint16(offs, true), 0xFF);
-        }
-
-        var palMode = palBlock >> 14;
-        var palOffs = (palBlock & 0x3FFF) << 2;
-
-        var palette = new Uint32Array(4);
-
-        palette[0] = getPal(palOffs + 0x00);
-        palette[1] = getPal(palOffs + 0x02);
+        const p1 = palView.getUint16(palOffs + 0x02, true);
+        colorTable[4] = expand5to8(p1 & 0x1F);
+        colorTable[5] = expand5to8((p1 >>> 5) & 0x1F);
+        colorTable[6] = expand5to8((p1 >>> 10) & 0x1F);
+        colorTable[7] = 0xFF;
 
         if (palMode === 0) {
             // PTY=0, A=0
-            palette[2] = getPal(palOffs + 0x04);
-            palette[3] = 0x00000000;
+            const p2 = palView.getUint16(palOffs + 0x04, true);
+            colorTable[8]  = expand5to8(p2 & 0x1F);
+            colorTable[9]  = expand5to8((p2 >>> 5) & 0x1F);
+            colorTable[10] = expand5to8((p2 >>> 10) & 0x1F);
+            colorTable[11] = 0xFF;
+            // Color4 is transparent black.
         } else if (palMode === 1) {
             // PTY=1, A=0
-            // Color2 is a blend of Color1/Color2.
-            palette[2] = mix(palette[0], palette[1]);
-            palette[3] = 0x00000000;
+            // Color3 is a blend of Color1/Color2.
+            colorTable[8]  = (colorTable[0] + colorTable[4]) >>> 1;
+            colorTable[9]  = (colorTable[1] + colorTable[5]) >>> 1;
+            colorTable[10] = (colorTable[2] + colorTable[6]) >>> 1;
+            colorTable[11] = 0xFF;
+            // Color4 is transparent black.
         } else if (palMode === 2) {
             // PTY=0, A=1
-            palette[2] = getPal(palOffs + 0x04);
-            palette[3] = getPal(palOffs + 0x06);
+            const p2 = palView.getUint16(palOffs + 0x04, true);
+            colorTable[8]  = expand5to8(p2 & 0x1F);
+            colorTable[9]  = expand5to8((p2 >>> 5) & 0x1F);
+            colorTable[10] = expand5to8((p2 >>> 10) & 0x1F);
+            colorTable[11] = 0xFF;
+
+            const p3 = palView.getUint16(palOffs + 0x04, true);
+            colorTable[12] = expand5to8(p3 & 0x1F);
+            colorTable[13] = expand5to8((p3 >>> 5) & 0x1F);
+            colorTable[14] = expand5to8((p3 >>> 10) & 0x1F);
+            colorTable[15] = 0xFF;
         } else {
-            palette[2] = mixs3(palette[0], palette[1]);
-            palette[3] = mixs3(palette[1], palette[0]);
+            colorTable[8]  = s3tcblend(colorTable[4], colorTable[0]);
+            colorTable[9]  = s3tcblend(colorTable[5], colorTable[1]);
+            colorTable[10] = s3tcblend(colorTable[6], colorTable[2]);
+            colorTable[11] = 0xFF;
+
+            colorTable[12] = s3tcblend(colorTable[0], colorTable[4]);
+            colorTable[13] = s3tcblend(colorTable[1], colorTable[5]);
+            colorTable[14] = s3tcblend(colorTable[2], colorTable[6]);
+            colorTable[15] = 0xFF;
         }
 
-        return palette;
+        return colorTable;
     }
 
-    var pixels = new Uint8Array(width * height * 4);
-    var texView = new DataView(texData);
-    var palView = new DataView(palData);
+    const pixels = new Uint8Array(width * height * 4);
+    const texView = new DataView(texData);
+    const palView = new DataView(palData);
 
-    var palIdxStart = (width * height) / 4;
+    const palIdxStart = (width * height) / 4;
 
-    var srcOffs = 0;
-    for (var yy = 0; yy < height; yy += 4) {
-        for (var xx = 0; xx < width; xx += 4) {
-            var texBlock = texView.getUint32((srcOffs * 0x04), true);
-            var palBlock = texView.getUint16(palIdxStart + (srcOffs * 0x02), true);
-            var palette = buildPalette(palBlock);
+    let srcOffs = 0;
+    for (let yy = 0; yy < height; yy += 4) {
+        for (let xx = 0; xx < width; xx += 4) {
+            let texBlock = texView.getUint32((srcOffs * 0x04), true);
+            const palBlock = texView.getUint16(palIdxStart + (srcOffs * 0x02), true);
+            const colorTable = buildColorTable(palBlock);
 
-            for (var y = 0; y < 4; y++) {
-                for (var x = 0; x < 4; x++) {
-                    var palIdx = texBlock & 0x03;
-                    var pixel = palette[palIdx];
-                    var dstPixel = ((yy + y) * width) + xx + x;
-                    writeColor(pixels, dstPixel, pixel);
+            for (let y = 0; y < 4; y++) {
+                for (let x = 0; x < 4; x++) {
+                    const colorIdx = texBlock & 0x03;
+                    const dstOffs = 4 * (((yy + y) * width) + xx + x);
+                    pixels[dstOffs+0] = colorTable[colorIdx*4+0];
+                    pixels[dstOffs+1] = colorTable[colorIdx*4+1];
+                    pixels[dstOffs+2] = colorTable[colorIdx*4+2];
+                    pixels[dstOffs+3] = colorTable[colorIdx*4+3];
                     texBlock >>= 2;
                 }
             }
@@ -177,36 +164,39 @@ function readTexture_CMPR_4x4(width:number, height:number, texData:ArrayBuffer, 
 }
 
 function readTexture_A5I3(width:number, height:number, texData:ArrayBuffer, palData:ArrayBuffer) {
-    var pixels = new Uint8Array(width * height * 4);
-    var texView = new DataView(texData);
-    var palView = new DataView(palData);
-    var srcOffs = 0;
+    const pixels = new Uint8Array(width * height * 4);
+    const texView = new DataView(texData);
+    const palView = new DataView(palData);
+    let srcOffs = 0;
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
-            var texBlock = texView.getUint8(srcOffs++);
-            var palIdx = (texBlock & 0x3) << 1;
-            var color = rgb5(palView.getUint16(palIdx, true), 0);
-            var alpha = texBlock >>> 3;
-            alpha = (alpha << (8-5)) | (alpha >>> (10-8));
-            var pixel = alpha << 24 | color;
-            var dstPixel = (y * width) + x;
-            writeColor(pixels, dstPixel, pixel);
-            texBlock >>= 2;
+            const texBlock = texView.getUint8(srcOffs++);
+            const palIdx = (texBlock & 0x03) << 1;
+            const alpha = texBlock >>> 3;
+            const p = palView.getUint16(palIdx, true);
+            const dstOffs = 4 * ((y * width) + x);
+            pixels[dstOffs+0] = expand5to8(p & 0x1F);
+            pixels[dstOffs+1] = expand5to8((p >>> 5) & 0x1F);
+            pixels[dstOffs+2] = expand5to8((p >>> 10) & 0x1F);
+            pixels[dstOffs+3] = expand5to8(alpha);
         }
     }
     return pixels;
 }
 
 function readTexture_Direct(width:number, height:number, texData:ArrayBuffer) {
-    var pixels = new Uint8Array(width * height * 4);
-    var texView = new DataView(texData);
-    var srcOffs = 0;
+    const pixels = new Uint8Array(width * height * 4);
+    const texView = new DataView(texData);
+    let srcOffs = 0;
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
-            var pixel = rgb5(texView.getUint16(srcOffs, true), 0xFF);
+            const p = texView.getUint16(srcOffs, true);
+            const dstOffs = 4 * ((y * width) + x);
+            pixels[dstOffs+0] = expand5to8(p & 0x1F);
+            pixels[dstOffs+1] = expand5to8((p >>> 5) & 0x1F);
+            pixels[dstOffs+2] = expand5to8((p >>> 10) & 0x1F);
+            pixels[dstOffs+3] = 0xFF;
             srcOffs += 2;
-            var dstPixel = (y * width) + x;
-            writeColor(pixels, dstPixel, pixel);
         }
     }
     return pixels;
