b42efa007b5c2d6f184e5395ee1f616c0f8044b5
Jasper St. Pierre <jstpierre@mecheye.net>
2018-01-15 03:03:08 -0800

    lz11

--- build/main.js
+++ build/main.js
@@ -34,11 +34,12 @@ var __read = (this && this.__read) || function (o, n) {
     }
     return ar;
 };
-// Nintendo DS LZ77 (LZ10) format.
+// Nintendo DS LZ77 format.
 System.register("lz77", [], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    // Header (8 bytes):
+    // LZ10:
+    // Header (4 bytes):
     //   Magic: "\x10" (1 byte)
     //   Uncompressed size (3 bytes, little endian)
     // Data:
@@ -51,11 +52,7 @@ System.register("lz77", [], function (exports_1, context_1) {
     //         Copy Length+3 bytes from Offset back in the output buffer.
     //     If flag is 0:
     //       Literal: copy one byte from src to dest.
-    function decompress(srcBuffer) {
-        var srcView = new DataView(srcBuffer);
-        var magic = srcView.getUint8(0x00);
-        if (magic !== 0x10)
-            throw new Error("Not Nintendo LZ77");
+    function decompressLZ10(srcView) {
         var uncompressedSize = srcView.getUint32(0x00, true) >> 8;
         var dstBuffer = new Uint8Array(uncompressedSize);
         var srcOffs = 0x04;
@@ -84,10 +81,85 @@ System.register("lz77", [], function (exports_1, context_1) {
             }
         }
     }
+    exports_1("decompressLZ10", decompressLZ10);
+    // LZ11:
+    // Header (4 bytes):
+    //   Magic: "\x11" (1 byte)
+    //   Uncompressed size (3 bytes, little endian)
+    // Data:
+    //   Flags (1 byte)
+    //   For each bit in the flags byte, from MSB to LSB:
+    //     If flag is 1:
+    //       Fancy LZ77. See below for more details. Flag switches on 4-7 of newly read byte.
+    //     If flag is 0:
+    //       Literal: copy one byte from src to dest.
+    function decompressLZ11(srcView) {
+        var uncompressedSize = srcView.getUint32(0x00, true) >> 8;
+        var dstBuffer = new Uint8Array(uncompressedSize);
+        var srcOffs = 0x04;
+        var dstOffs = 0x00;
+        while (true) {
+            var commandByte = srcView.getUint8(srcOffs++);
+            var i = 8;
+            while (i--) {
+                if (commandByte & (1 << i)) {
+                    var tmp = srcView.getUint32(srcOffs, false);
+                    var windowOffset = void 0, windowLength = void 0;
+                    var indicator = (tmp >>> 28);
+                    if (indicator > 1) {
+                        // Two bytes. AB CD xx xx
+                        // Length: A + 1
+                        // Offset: BCD + 1
+                        windowLength = indicator + 1;
+                        windowOffset = ((tmp >>> 16) & 0x0FFF) + 1;
+                    }
+                    else if (indicator === 0) {
+                        // Three bytes: AB CD EF xx
+                        // Length: BC + 0x11
+                        // Offset: DEF + 1
+                        windowLength = (tmp >>> 20) + 0x11;
+                        windowOffset = ((tmp >>> 8) & 0x0FFF) + 1;
+                        srcOffs += 3;
+                    }
+                    else if (indicator === 1) {
+                        // Four bytes. AB CD EF GH
+                        // Length: BCDE + 0x11
+                        // Offset: FGH + 1
+                        windowLength = ((tmp >>> 12) & 0xFFFF) + 0x111;
+                        windowOffset = (tmp & 0x0FFF) + 1;
+                        srcOffs += 4;
+                    }
+                    var copyOffs = dstOffs - windowOffset;
+                    uncompressedSize -= windowLength;
+                    while (windowLength--)
+                        dstBuffer[dstOffs++] = dstBuffer[copyOffs++];
+                }
+                else {
+                    // Literal.
+                    uncompressedSize--;
+                    dstBuffer[dstOffs++] = srcView.getUint8(srcOffs++);
+                }
+                if (uncompressedSize <= 0)
+                    return dstBuffer.buffer;
+            }
+        }
+    }
+    exports_1("decompressLZ11", decompressLZ11);
+    function decompress(srcBuffer) {
+        var srcView = new DataView(srcBuffer);
+        var magic = srcView.getUint8(0x00);
+        if (magic === 0x10)
+            return decompressLZ10(srcView);
+        else if (magic === 0x11)
+            return decompressLZ11(srcView);
+        else
+            throw new Error("Not Nintendo LZ77");
+    }
     exports_1("decompress", decompress);
     return {
         setters: [],
-        execute: function () {// Nintendo DS LZ77 (LZ10) format.
+        execute: function () {
+            // Nintendo DS LZ77 format.
         }
     };
 });
