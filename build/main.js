6e83ecd0ba913de329cc81dbfe17a8d8fa8ddd5c
Jasper St. Pierre <jstpierre@mecheye.net>
2018-01-16 01:01:52 -0800

    sm64ds clears to black

--- build/main.js
+++ build/main.js
@@ -4177,6 +4177,9 @@ System.register("sm64ds/render", ["gl-matrix", "viewer", "sm64ds/crg0", "sm64ds/
                     gl.enable(gl.DEPTH_TEST);
                     gl.enable(gl.BLEND);
                     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                    // Clear to black.
+                    gl.clearColor(0, 0, 0, 1.0);
+                    gl.clear(gl.COLOR_BUFFER_BIT);
                     // First pass, opaque.
                     this.renderModels(state, RenderPass.OPAQUE);
                     // Second pass, translucent.
