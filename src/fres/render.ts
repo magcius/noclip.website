a066cdbb1c7d3a82ba4951f16b90fb55c67e1e5b
Jasper St. Pierre <jstpierre@mecheye.net>
2018-01-18 05:21:42 -0800

    Initial work on BFRES
    The Wii U version as seen in Splatoon, at least. Textures seem to be working correctly.

---
+++ src/fres/render.ts
@@ -0,0 +1,52 @@
+
+import * as Viewer from 'viewer';
+import * as Yaz0 from 'yaz0';
+
+import * as GX2Texture from './gx2_texture';
+import * as BFRES from './bfres';
+import * as SARC from './sarc';
+
+import { assert, fetch } from 'util';
+
+export class Scene implements Viewer.Scene {
+    public cameraController = Viewer.FPSCameraController;
+    public textures: HTMLCanvasElement[];
+
+    private fres: BFRES.FRES;
+
+    constructor(fres: BFRES.FRES) {
+        this.fres = fres;
+
+        this.textures = this.fres.textures.map((textureEntry) => {
+            const canvas = GX2Texture.textureToCanvas(textureEntry.texture);
+            canvas.title = `${textureEntry.name} ${textureEntry.texture.type}`;
+            return canvas;
+        });
+    }
+
+    public render(state: Viewer.RenderState) {
+    }
+}
+
+export class SceneDesc implements Viewer.SceneDesc {
+    public id: string;
+    public name: string;
+    public path: string;
+
+    constructor(name: string, path: string) {
+        this.name = name;
+        this.path = path;
+        this.id = this.path;
+    }
+
+    public createScene(gl: WebGLRenderingContext): PromiseLike<Scene> {
+        return fetch(this.path).then((result: ArrayBuffer) => {
+            const buf = Yaz0.decompress(result);
+            const sarc = SARC.parse(buf);
+            const file = sarc.files.find((file) => file.name === 'Output.bfres');
+            const fres = BFRES.parse(file.buffer);
+            const scene = new Scene(fres);
+            return scene;
+        });
+    }
+}
