f321995c637167f2c56be837770963db28a95a31
Jasper St. Pierre <jstpierre@mecheye.net>
2018-08-27 20:58:26 -0700

    fres: Fix swizzle worker
    Bad tsconfig. Oops. Fixes Splatoon.

--- src/fres/worker/gx2_swizzle_worker.ts
+++ src/fres/worker/gx2_swizzle_worker.ts
@@ -4,5 +4,5 @@ import { DeswizzleRequest, deswizzle } from "../gx2_swizzle";
 onmessage = (e: MessageEvent) => {
     const req: DeswizzleRequest = e.data;
     const deswizzledSurface = deswizzle(req.surface, req.buffer, req.mipLevel);
-    postMessage(deswizzledSurface, null, [deswizzledSurface.pixels]);
+    postMessage(deswizzledSurface, [deswizzledSurface.pixels]);
 };
