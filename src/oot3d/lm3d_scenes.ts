dc1ba437488834c1d2f0ae331e8bd95fc9e144b1
SpaceCats64 <spacecats64@gmail.com>
2018-10-13 18:21:32 -0400

    updated bcsv and map loading for LM3D

--- src/oot3d/lm3d_scenes.ts
+++ src/oot3d/lm3d_scenes.ts
@@ -3,11 +3,13 @@
 
 import * as CMB from './cmb';
 import * as ZAR from './zar';
+import * as BCSV from '../luigis_mansion/bcsv';
 
 import * as Viewer from '../viewer';
 import * as UI from '../ui';
 
 import Progressable from '../Progressable';
+import { mat4 } from 'gl-matrix';
 import { CtrTextureHolder, CmbRenderer } from './render';
 import { SceneGroup } from '../viewer';
 import { fetchData } from '../fetch';
@@ -46,20 +48,36 @@ class SceneDesc implements Viewer.SceneDesc {
 
     public createScene(gl: WebGL2RenderingContext): Progressable<Viewer.MainScene> {
         // Fetch the ZAR & info ZSI.
-        const path_gar = `data/lm3d/${this.id}.gar`;
+        
+        const path_gar = `data/lm3d/map/${this.id}.gar`;
+        //spacecats: this is pretty hacky, but it works
+        const models_path = Number.parseInt(this.id.split('p')[1]) < 10 ? `data/lm3d/mapmdl/${this.id.replace('0','')}/` : `data/lm3d/mapmdl/${this.id}/`;
+        const rooms: CmbRenderer[] = [];
+        const textureHolder = new CtrTextureHolder();
+
         return fetchData(path_gar).then((garBuffer) => {
             const gar = ZAR.parse(garBuffer);
-            const roomGarFile = gar.files.find((file) => file.name === 'room.gar');
-            const roomGar = ZAR.parse(roomGarFile.buffer);
 
-            const textureHolder = new CtrTextureHolder();
+            const jmpGarFile = gar.files.find((file) => file.name === "JMP.gar");
+            const jmpGar = ZAR.parse(jmpGarFile.buffer);
+            const roomInfoFile = jmpGar.files.find((file) => file.name === "RoomInfo.gseb");
+            const roomInfo =  BCSV.parse(roomInfoFile.buffer, true);
 
-            const firstCMB = roomGar.files.find((file) => file.name.endsWith('.cmb'));
+            for(let i = 0; i < roomInfo.records.length; i++){
+                fetchData(models_path + ( i < 10 ? `room_0${i}.gar` : `room_${i}.gar`)).then((roomArcBuff) => {
+                    
+                    const roomFile = ZAR.parse(roomArcBuff);
+                    const roomGarFile = roomFile.files.find((file) => file.name === 'room.gar');
+                    const roomGar = ZAR.parse(roomGarFile.buffer);
+                    const firstCMB = roomGar.files.find((file) => file.name.endsWith('.cmb'));
+                    const cmb = CMB.parse(firstCMB.buffer);
+
+                    const cmbRenderer = new CmbRenderer(gl, textureHolder, cmb, cmb.name);
+                    rooms.push(cmbRenderer);
+                });
+            }
     
-            const cmb = CMB.parse(firstCMB.buffer);
-            console.log(cmb);
-            const cmbRenderer = new CmbRenderer(gl, textureHolder, cmb, cmb.name);
-            return cmbRenderer;
+            return new MultiScene(rooms, textureHolder);
         });
     }
 }
@@ -88,7 +106,15 @@ export function createSceneFromGARBuffer(gl: WebGL2RenderingContext, buffer: Arr
 const id = "lm3d";
 const name = "Luigi's Mansion 3D";
 const sceneDescs: SceneDesc[] = [
-    { id: "mapmdl/map1/room_00" },
+    { id: "map01" },
+    { id: "map02" },
+    { id: "map03" },
+    { id: "map04" },
+    { id: "map09" },
+    { id: "map10" },
+    { id: "map11" },
+    { id: "map12" },
+    { id: "map13" },
 ].map((entry): SceneDesc => {
     const name = entry.id;
     return new SceneDesc(name, entry.id);
