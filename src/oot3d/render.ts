25a53036402c3886e71c828a7bae89554f2d028f
Jasper St. Pierre <jstpierre@mecheye.net>
2019-03-17 22:40:37 -0700

    oot3d: er, a touchup to fix lighting

--- src/oot3d/render.ts
+++ src/oot3d/render.ts
@@ -397,12 +397,12 @@ void main() {
     v_Color.rgb = Monochrome(v_Color.rgb);
 #endif
 
+#ifdef USE_LIGHTING
     vec3 t_Lighting = AMBIENT_LIGHT_COLOR * 2.0;
     t_Lighting += clamp(dot(-a_Normal, PRIMARY_LIGHT_DIRECTION), 0.0, 1.0) * PRIMARY_LIGHT_COLOR;
     t_Lighting += clamp(dot(-a_Normal, SECONDARY_LIGHT_DIRECTION), 0.0, 1.0) * SECONDARY_LIGHT_COLOR;
 
-#ifdef USE_LIGHTING
-    v_Color *= t_Lighting;
+    v_Color.rgb *= t_Lighting;
 #endif
 
     v_TexCoord0 = ${this.generateVertexCoord(0)};
