64bf06345f8d33e1401d04c62ac27e8d64931198
krakenkrakenkrakenkraken <thekrakensixtyfour@gmail.com>
2019-04-18 21:58:11 -0400

    added more galaxies

--- src/j3d/smg1_scenes.ts
+++ src/j3d/smg1_scenes.ts
@@ -13,16 +13,23 @@ const id = "smg";
 const name = "Super Mario Galaxy";
 
 const sceneDescs: Viewer.SceneDesc[] = [
-    new SMG1SceneDesc("Peach's Castle Garden", "PeachCastleGardenGalaxy"),
+    "Observatory"
     new SMG1SceneDesc("Comet Observatory", "AstroGalaxy"),
-    new SMG1SceneDesc("Battlerock Galaxy", "BattleShipGalaxy"),
+    new SMG1SceneDesc("Peach's Castle Garden", "PeachCastleGardenGalaxy"),
+    "Terrace"
+    new SMG1SceneDesc("Good Egg Galaxy", "EggStarGalaxy"),
     new SMG1SceneDesc("Honeyhive Galaxy", "HoneyBeeKingdomGalaxy"),
     new SMG1SceneDesc("Sweet Sweet Galaxy", "BeltConveyerExGalaxy"),
-    new SMG1SceneDesc("Good Egg Galaxy", "EggStarGalaxy"),
-    new SMG1SceneDesc("Beach Bowl Galaxy", "HeavenlyBeachGalaxy"),
+    "Fountain"
     new SMG1SceneDesc("Space Junk Galaxy", "StarDustGalaxy"),
-    new SMG1SceneDesc("Dusty Dune Galaxy", "SandClockGalaxy"),
+    new SMG1SceneDesc("Battlerock Galaxy", "BattleShipGalaxy"),
+    "Kitchen"
+    new SMG1SceneDesc("Beach Bowl Galaxy", "HeavenlyBeachGalaxy"),
     new SMG1SceneDesc("Ghostly Galaxy", "PhantomGalaxy"),
+    "Bedroom"
+    new SMG1SceneDesc("Dusty Dune Galaxy", "SandClockGalaxy"),
+    "Engine Room"
+    "Garden"
     new SMG1SceneDesc("Melty Molten Galaxy", "HellProminenceGalaxy"),
 ];
 
