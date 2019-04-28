d38a0febcb24b04bc9879d6bf9d8644d25072275
Jasper St. Pierre <jstpierre@mecheye.net>
2019-04-28 16:18:28 -0700

    BuildVersion: Actually use a string

--- src/BuildVersion.ts
+++ src/BuildVersion.ts
@@ -4,7 +4,7 @@
 //@ts-ignore
 import { readFileSync } from 'fs';
 //@ts-ignore
-export const GIT_REVISION: string = readFileSync(__dirname + '/../.git/refs/heads/master');
+export const GIT_REVISION: string = readFileSync(__dirname + '/../.git/refs/heads/master', { encoding: 'utf8' });
 export const GIT_SHORT_REVISION = GIT_REVISION.slice(0, 8);
 export const GITHUB_URL = `https://github.com/magcius/noclip.website`;
 export const GITHUB_REVISION_URL = `${GITHUB_URL}/commit/${GIT_REVISION}`;
