e89abda25cbb8803c268cf8b27eb787e376c5043
David A Roberts <d@vidr.cc>
2019-10-01 18:22:26 +1000

    Add GTA3

---
+++ src/gta3/scenes.ts
@@ -0,0 +1,145 @@
+import * as Viewer from '../viewer';
+import * as rw from 'librw';
+import { GfxDevice } from '../gfx/platform/GfxPlatform';
+import { DataFetcher } from '../DataFetcher';
+import { GTA3Renderer, SceneRenderer } from './render';
+import { SceneContext } from '../SceneBase';
+import { getTextDecoder, assert } from '../util';
+import { parseItemPlacement, ItemPlacement, parseItemDefinition, ItemDefinition } from './item';
+import { quat, vec3 } from 'gl-matrix';
+
+class GTA3SceneDesc implements Viewer.SceneDesc {
+    private static initialised = false;
+    constructor(public id: string, public name: string) {
+    }
+
+    private static async initialise() {
+        if (this.initialised) return;
+        await rw.init({ gtaPlugins: true, platform: rw.Platform.PLATFORM_D3D8 });
+        rw.Texture.setCreateDummies(true);
+        rw.Texture.setLoadTextures(false);
+        this.initialised = true;
+    }
+
+    private async fetchTXD(id: string, dataFetcher: DataFetcher): Promise<rw.TexDictionary> {
+        let path = (id === 'generic') ? 'gta3/models/generic.txd' : `gta3/models/gta3/${id}.txd`;
+        let buffer = await dataFetcher.fetchData(path);
+        let stream = new rw.StreamMemory(buffer.arrayBuffer);
+        let header = new rw.ChunkHeaderInfo(stream);
+        assert(header.type === rw.PluginID.ID_TEXDICTIONARY);
+        let txd = new rw.TexDictionary(stream);
+        header.delete();
+        stream.delete();
+        return txd;
+    }
+
+    private async fetchDFF(id: string, dataFetcher: DataFetcher): Promise<rw.Clump> {
+        let path = `gta3/models/gta3/${id}.dff`;
+        let buffer = await dataFetcher.fetchData(path);
+
+        let stream = new rw.StreamMemory(buffer.arrayBuffer);
+        let header = new rw.ChunkHeaderInfo(stream);
+
+        assert(header.type === rw.PluginID.ID_CLUMP);
+        let clump = rw.Clump.streamRead(stream);
+
+        header.delete();
+        stream.delete();
+        return clump;
+    }
+
+
+    private async fetchIDE(name: string, dataFetcher: DataFetcher): Promise<ItemDefinition> {
+        const buffer = await dataFetcher.fetchData(`gta3/data/maps/${name}`);
+        const text = getTextDecoder('utf8')!.decode(buffer.arrayBuffer);
+        return parseItemDefinition(text);
+    }
+
+    private async fetchIPL(id: string, dataFetcher: DataFetcher): Promise<ItemPlacement> {
+        if (id === 'test') return {
+            instances: [{
+                id: 0,
+                modelName: 'billboard01',
+                rotation: quat.fromValues(0,0,0,1),
+                translation: vec3.fromValues(0,0,0),
+                scale: vec3.fromValues(10,10,10),
+            }]
+        };
+        const buffer = await dataFetcher.fetchData(`gta3/data/maps/${id}.ipl`);
+        const text = getTextDecoder('utf8')!.decode(buffer.arrayBuffer);
+        return parseItemPlacement(text);
+    }
+
+    public async createScene(device: GfxDevice, context: SceneContext): Promise<Viewer.SceneGfx> {
+        await GTA3SceneDesc.initialise();
+        const dataFetcher = context.dataFetcher;
+        const modelTXD = new Map<String, string>();
+
+        const gta3IDE = await this.fetchIDE('gta3.IDE', dataFetcher);
+        for (const obj of gta3IDE.objects) modelTXD.set(obj.modelName, obj.txdName);
+        if (this.id.match(/\//)) {
+            const ide = await this.fetchIDE(this.id.toLowerCase() + '.ide', dataFetcher);
+            for (const obj of ide.objects) modelTXD.set(obj.modelName, obj.txdName);
+        }
+
+        const renderer = new GTA3Renderer(device);
+        const sceneRenderer = new SceneRenderer();
+        const ipl = await this.fetchIPL(this.id, dataFetcher);
+        const loaded = new Map<String, Promise<void>>();
+        for (const item of ipl.instances) {
+            const name = item.modelName;
+            if (name.startsWith('lod')) continue; // ignore LOD objects
+
+            const txdName = modelTXD.get(name);
+            if (!txdName) {
+                console.warn('Cannot find textures for model', name);
+                continue;
+            }
+
+            if (!loaded.has(name + '.dff')) {
+                let txdLoaded = loaded.get(txdName + '.txd');
+                if (!txdLoaded) {
+                    txdLoaded = this.fetchTXD(txdName, dataFetcher).then(txd => {
+                        renderer.textureHolder.addTXD(device, txd);
+                        txd.delete();
+                    });
+                    loaded.set(txdName + '.txd', txdLoaded);
+                }
+                loaded.set(name + '.dff', this.fetchDFF(name, dataFetcher).then(async dff => {
+                    await txdLoaded;
+                    sceneRenderer.addModel(device, renderer.textureHolder, name, dff);
+                    dff.delete();
+                }));
+            }
+        }
+
+        for (const item of ipl.instances) {
+            const dffLoaded = loaded.get(item.modelName + '.dff');
+            if (dffLoaded) dffLoaded.then(() => sceneRenderer.addItem(item));
+        }
+        renderer.sceneRenderers.push(sceneRenderer);
+        return renderer;
+    }
+}
+
+const id = "gta3";
+const name = "Grand Theft Auto III";
+const sceneDescs = [
+    //new GTA3SceneDesc("test", "Test"),
+    new GTA3SceneDesc("overview", "Overview"),
+    "Portland",
+    new GTA3SceneDesc("industne/industNE", "North-east"),
+    new GTA3SceneDesc("industnw/industNW", "North-west"),
+    new GTA3SceneDesc("industse/industSE", "South-east"),
+    new GTA3SceneDesc("industsw/industSW", "South-west"),
+    "Staunton Island",
+    new GTA3SceneDesc("comntop/comNtop", "North"),
+    new GTA3SceneDesc("comnbtm/comNbtm", "Central"),
+    new GTA3SceneDesc("comse/comSE", "South-east"),
+    new GTA3SceneDesc("comsw/comSW", "South-west"),
+    "Shoreside Vale",
+    new GTA3SceneDesc("landne/landne", "North-east"),
+    new GTA3SceneDesc("landsw/landsw", "South-west"),
+];
+
+export const sceneGroup: Viewer.SceneGroup = { id, name, sceneDescs };
