0918b91b768668e25b2ec79f6f61df0d3ab700ae
Luke Street <luke.street@encounterpc.com>
2019-11-19 22:29:57 -0500

    Replace with thread-loader and cache-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const HappyPack = require('happypack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = merge(common, {
@@ -15,29 +13,31 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.ts$/,
-        loader: 'happypack/loader?id=ts',
         exclude: /node_modules/,
+        use: [
+          // Cache intermediate results
+          {loader: 'cache-loader'},
+          // Run ts-loader in parallel, leaving one CPU for checker
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: require('os').cpus().length - 1,
+              poolTimeout: Infinity, // set to Infinity in watch mode
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    // Cache intermediate results
-    new HardSourceWebpackPlugin(),
-    // Run ts-loader in parallel, leaving one CPU for checker
-    new HappyPack({
-      id: 'ts',
-      threads: require('os').cpus().length - 1,
-      use: [
-        {
-          path: 'ts-loader',
-          query: {
-            happyPackMode: true,
-          },
-        },
-      ],
     }),
     // Run ts checker asynchronously
     new ForkTsCheckerWebpackPlugin({
