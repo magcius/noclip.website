31eb254e18815f5e995fb2acc940641d09de941e
Luke S <luke.street@encounterpc.com>
2019-11-19 22:34:15 -0500

    webpack: Improvements to dev server speed (#179)
    * webpack: Improvements to dev server speed
    
    * Use cheap-module-eval-source-map devtool
    
    * Replace with thread-loader and cache-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,47 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          // Cache intermediate results
+          {loader: 'cache-loader'},
+          // Run ts-loader in parallel, leaving one CPU for checker
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: require('os').cpus().length - 1,
+              poolTimeout: Infinity, // set to Infinity in watch mode
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    // Run ts checker asynchronously
+    new ForkTsCheckerWebpackPlugin({
+      checkSyntacticErrors: true,
     }),
   ],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,15 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
