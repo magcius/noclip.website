7f76477cea86012a9251eb730bced4bc582a0555
Mike Lester <themikelester@gmail.com>
2019-12-24 14:48:47 -0500

    !== not !=

--- src/gx/gx_displaylist.ts
+++ src/gx/gx_displaylist.ts
@@ -1045,7 +1045,7 @@ export class DisplayListRegisters {
         // Retrieve existing value, overwrite w/ mask.
         const regValue = (this.bp[regAddr] & ~regWMask) | (regBag & regWMask);
         // The mask resets after use.
-        if (regAddr != GX.BPRegister.SS_MASK) 
+        if (regAddr !== GX.BPRegister.SS_MASK) 
             this.bp[GX.BPRegister.SS_MASK] = 0x00FFFFFF;
         // Set new value.
         this.bp[regAddr] = regValue;
