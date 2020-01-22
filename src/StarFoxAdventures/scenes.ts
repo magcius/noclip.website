13a7aabfd6b857117fc9fc1a96dee889cd8ae1fb
N.E.C <beholdnec@gmail.com>
2020-01-22 15:23:04 -0800

    SFA: More work on vertex loading

--- src/StarFoxAdventures/scenes.ts
+++ src/StarFoxAdventures/scenes.ts
@@ -6,10 +6,10 @@ import { GfxRenderCache } from '../gfx/render/GfxRenderCache';
 import { SceneContext } from '../SceneBase';
 import * as GX_Material from '../gx/gx_material';
 
-import { hexzero } from '../util';
+import { hexzero, nArray } from '../util';
 import * as GX from '../gx/gx_enum';
 import { BasicGXRendererHelper, fillSceneParamsDataOnTemplate, GXShapeHelperGfx, loadedDataCoalescerComboGfx, PacketParams, GXMaterialHelperGfx, MaterialParams } from '../gx/gx_render';
-import { GX_VtxDesc, GX_VtxAttrFmt, compileLoadedVertexLayout, compileVtxLoader, LoadedVertexLayout, LoadedVertexData, GX_Array, getAttributeByteSize } from '../gx/gx_displaylist';
+import { GX_VtxDesc, GX_VtxAttrFmt, compileLoadedVertexLayout, compileVtxLoaderMultiVat, LoadedVertexLayout, LoadedVertexData, GX_Array, getAttributeByteSize } from '../gx/gx_displaylist';
 import ArrayBufferSlice from '../ArrayBufferSlice';
 import { Camera, computeViewMatrix } from '../Camera';
 import { mat4 } from 'gl-matrix';
@@ -23,7 +23,7 @@ class ModelInstance {
     shapeHelper: GXShapeHelperGfx | null = null;
     materialHelper: GXMaterialHelperGfx;
 
-    constructor(verts: Int16Array) {
+    constructor(verts: Int16Array, vtxArrays: GX_Array[], vcd: GX_VtxDesc[], vat: GX_VtxAttrFmt[][], displayList: ArrayBufferSlice) {
         this.verts = verts;
 
         const gxMaterial: GX_Material.GXMaterial = {
@@ -73,30 +73,14 @@ class ModelInstance {
         };
         this.materialHelper = new GXMaterialHelperGfx(gxMaterial);
 
-        const vcd: GX_VtxDesc[] = [];
-        const vat: GX_VtxAttrFmt[][] = [[]];
-        for (let i = 0; i <= GX.Attr.MAX; i++) {
-            vcd[i] = { type: GX.AttrType.NONE };
-            vat[0][i] = { compType: GX.CompType.F32, compShift: 0, compCnt: 0 };
-        }
-        vcd[GX.Attr.POS].type = GX.AttrType.DIRECT;
-        vat[0][GX.Attr.POS] = { compType: GX.CompType.S16, compShift: 0, compCnt: GX.CompCnt.POS_XYZ };
-
-        const vtxLoader = compileVtxLoader(vat[0], vcd);
+        const vtxLoader = compileVtxLoaderMultiVat(vat, vcd);
         this.loadedVertexLayout = vtxLoader.loadedVertexLayout;
 
-        const vtxArrays: GX_Array[] = [];
-        const vertsBuffer = new ArrayBufferSlice(verts.buffer);
-        vtxArrays[GX.Attr.POS] = { buffer: vertsBuffer, offs: 0, stride: getAttributeByteSize(vat[0], GX.Attr.POS) };
-        const numVerts = (this.verts.length / 3)|0;
-        const dl = new DataView(new ArrayBuffer(3 + 3*2*numVerts + 1));
-        dl.setUint8(0, GX.Command.DRAW_TRIANGLES | 0); // Command
-        dl.setUint16(1, this.verts.length / 3); // Vertex count
-        for (let i = 0; i < this.verts.length; i++) {
-            dl.setInt16(3 + i*2, this.verts[i]);
-        }
-        dl.setUint8(3 + this.verts.length*2, 0); // End
-        this.loadedVertexData = vtxLoader.runVertices(vtxArrays, new ArrayBufferSlice(dl.buffer));
+        // const vtxArrays: GX_Array[] = [];
+        // const vertsBuffer = new ArrayBufferSlice(verts.buffer);
+        // vtxArrays[GX.Attr.POS] = { buffer: vertsBuffer, offs: 0, stride: getAttributeByteSize(vat[0], GX.Attr.POS) };
+        // this.loadedVertexData = vtxLoader.runVertices(vtxArrays, new ArrayBufferSlice(dl.buffer));
+        this.loadedVertexData = vtxLoader.runVertices(vtxArrays, displayList);
         console.log(`loaded vertex data ${JSON.stringify(this.loadedVertexData, null, '\t')}`);
     }
 
@@ -146,9 +130,9 @@ class ModelInstance {
 class SFARenderer extends BasicGXRendererHelper {
     model: ModelInstance;
 
-    constructor(device: GfxDevice, verts: Int16Array) {
+    constructor(device: GfxDevice, verts: Int16Array, vtxArrays: GX_Array[], vcd: GX_VtxDesc[], vat: GX_VtxAttrFmt[][], displayList: ArrayBufferSlice) {
         super(device);
-        this.model = new ModelInstance(verts);
+        this.model = new ModelInstance(verts, vtxArrays, vcd, vat, displayList);
     }
 
     protected prepareToRender(device: GfxDevice, hostAccessPass: GfxHostAccessPass, viewerInput: Viewer.ViewerRenderInput): void {
@@ -180,6 +164,42 @@ class ZLBHeader {
 
 function stringToFourCC(s: string): number {
     return (s.charCodeAt(0) << 24) | (s.charCodeAt(1) << 16) | (s.charCodeAt(2) << 8) | s.charCodeAt(3)
+}
+
+// Reads bitfields by pulling from the low bits of each byte in sequence
+class LowBitReader {
+    dv: DataView
+    offs: number
+    num: number
+    buf: number
+
+    constructor(dv: DataView, offs: number = 0) {
+        this.dv = dv;
+        this.offs = offs;
+        this.num = 0;
+        this.buf = 0;
+    }
+
+    peek(bits: number): number {
+        while (this.num < bits) {
+            this.buf |= this.dv.getUint8(this.offs) << this.num
+            this.offs++;
+            this.num += 8;
+        }
+        return this.buf & ((1<<bits)-1);
+    }
+
+    drop(bits: number) {
+        this.peek(bits); // Ensure buffer has bits to drop
+        this.buf >>>= bits
+        this.num -= bits
+    }
+
+    get(bits: number): number {
+        const x = this.peek(bits)
+        this.drop(bits)
+        return x
+    }
 }
 
 class SFASceneDesc implements Viewer.SceneDesc {
@@ -216,7 +236,142 @@ class SFASceneDesc implements Viewer.SceneDesc {
             offs += 0x06;
         }
 
-        const renderer = new SFARenderer(device, verts);
+        const polyOffset = uncompDv.getUint32(0x64);
+        const polyCount = uncompDv.getUint8(0xA1);
+        console.log(`Loading ${polyCount} polygons from 0x${polyOffset.toString(16)}`);
+
+        interface PolygonType {
+            hasNormal: boolean;
+            hasColor: boolean;
+            hasTexCoord: boolean[];
+        }
+
+        const polyTypes: PolygonType[] = [];
+        offs = polyOffset;
+        for (let i = 0; i < polyCount; i++) {
+            const polyType = {
+                hasNormal: false,
+                hasColor: false,
+                hasTexCoord: nArray(8, () => false),
+            };
+            console.log(`parsing polygon attributes ${i}`);
+            const unk8 = uncompDv.getUint32(offs + 8);
+            console.log(`unk8 (flag 0x1): ${unk8}`);
+            const unk14 = uncompDv.getUint32(offs + 0x14);
+            console.log(`unk14 (flag 0x2): ${unk14}`);
+            const attrFlags = uncompDv.getUint8(offs + 0x40);
+            polyType.hasNormal = (attrFlags & 1) != 0;
+            polyType.hasColor = (attrFlags & 2) != 0;
+            const numTexCoords = uncompDv.getUint8(offs + 0x41);
+            if (attrFlags & 4) {
+                for (let j = 0; j < numTexCoords; j++) {
+                    polyType.hasTexCoord[j] = true;
+                }
+            }
+            const unk42 = uncompDv.getUint8(offs + 0x42);
+            console.log(`attrFlags 0x${hexzero(attrFlags, 2)}, numTexCoords ${numTexCoords}, unk42 0x${hexzero(unk42, 2)}`);
+            polyTypes.push(polyType);
+            offs += 0x44;
+        }
+        
+        const vcd: GX_VtxDesc[] = [];
+        const vat: GX_VtxAttrFmt[][] = nArray(8, () => []);
+        for (let i = 0; i <= GX.Attr.MAX; i++) {
+            vcd[i] = { type: GX.AttrType.NONE };
+            for (let j = 0; j < 8; j++) {
+                vat[j][i] = { compType: GX.CompType.F32, compShift: 0, compCnt: 0 };
+            }
+        }
+        vcd[GX.Attr.POS].type = GX.AttrType.INDEX16;
+        for (let i = 0; i < 8; i++) {
+            vat[i][GX.Attr.POS] = { compType: GX.CompType.S16, compShift: 0, compCnt: GX.CompCnt.POS_XYZ };
+        }
+
+        const chunkOffset = uncompDv.getUint32(0x68);
+        console.log(`chunkOffset 0x${chunkOffset.toString(16)}`);
+
+        const bitsOffset = uncompDv.getUint32(0x78);
+        const bitsCount = uncompDv.getUint16(0x84);
+        console.log(`Loading ${bitsCount} bits from 0x${bitsOffset.toString(16)}`);
+
+        let displayList = new ArrayBufferSlice(new ArrayBuffer(1));
+
+        const bits = new LowBitReader(uncompDv, bitsOffset);
+        let done = false;
+        let curPolyType = 0;
+        // setPolyType(curPolyType);
+        while (!done) {
+            const opcode = bits.get(4);
+            switch (opcode) {
+            case 1: // Set polygon type
+                curPolyType = bits.get(6);
+                // setPolyType(curPolyType);
+                console.log(`setting poly type ${curPolyType}`);
+                break;
+            case 2: // Geometry
+                const chunkNum = bits.get(8);
+                console.log(`geometry chunk #${chunkNum}`);
+                offs = chunkOffset + chunkNum * 0x1C;
+                const dlOffset = uncompDv.getUint32(offs);
+                const dlSize = uncompDv.getUint16(offs + 4);
+                displayList = new ArrayBufferSlice(uncompDv.buffer, dlOffset, dlSize);
+                // displayList = uncompDv.buffer.slice(dlOffset, dlOffset + dlSize);
+                console.log(`DL offset 0x${dlOffset.toString(16)} size 0x${dlSize.toString(16)}`);
+                // XXX: finish now
+                done = true;
+                break;
+            case 3: // Set vertex attributes
+                const posDesc = bits.get(1);
+                console.log(`posDesc ${posDesc}`);
+                vcd[GX.Attr.POS].type = posDesc ? GX.AttrType.INDEX16 : GX.AttrType.INDEX8;
+                if (polyTypes[curPolyType].hasNormal) {
+                    const normalDesc = bits.get(1);
+                    console.log(`normalDesc ${normalDesc}`);
+                    vcd[GX.Attr.NRM].type = normalDesc ? GX.AttrType.INDEX16 : GX.AttrType.INDEX8;
+                } else {
+                    vcd[GX.Attr.NRM].type = GX.AttrType.NONE;
+                }
+                if (polyTypes[curPolyType].hasColor) {
+                    const colorDesc = bits.get(1);
+                    console.log(`colorDesc ${colorDesc}`);
+                    vcd[GX.Attr.CLR0].type = colorDesc ? GX.AttrType.INDEX16 : GX.AttrType.INDEX8;
+                } else {
+                    vcd[GX.Attr.CLR0].type = GX.AttrType.NONE;
+                }
+                if (polyTypes[curPolyType].hasTexCoord[0]) {
+                    const texCoordDesc = bits.get(1);
+                    console.log(`texCoordDesc: ${texCoordDesc}`);
+                    // Note: texCoordDesc applies to all texture coordinates in the vertex
+                    for (let t = 0; t < 8; t++) {
+                        if (polyTypes[curPolyType].hasTexCoord[t]) {
+                            vcd[GX.Attr.TEX0 + t].type = texCoordDesc ? GX.AttrType.INDEX16 : GX.AttrType.INDEX8;
+                        } else {
+                            vcd[GX.Attr.TEX0 + t].type = GX.AttrType.NONE;
+                        }
+                    }
+                }
+                break;
+            case 4: // Set weights
+                const numWeights = bits.get(4);
+                for (let i = 0; i < numWeights; i++) {
+                    const weight = bits.get(8);
+                    console.log(`weight ${i}: ${weight}`);
+                }
+                break;
+            case 5: // End
+                done = true;
+                break;
+            default:
+                throw Error(`Unknown model bits opcode ${opcode}`);
+            }
+        }
+
+        const vtxArrays: GX_Array[] = [];
+        vtxArrays[GX.Attr.POS] = { buffer: new ArrayBufferSlice(verts.buffer), offs: 0, stride: getAttributeByteSize(vat[0], GX.Attr.POS) };
+        vtxArrays[GX.Attr.CLR0] = { buffer: new ArrayBufferSlice(new ArrayBuffer(0x10000)), offs: 0, stride: getAttributeByteSize(vat[0], GX.Attr.CLR0) };
+        vtxArrays[GX.Attr.TEX0] = { buffer: new ArrayBufferSlice(new ArrayBuffer(0x10000)), offs: 0, stride: getAttributeByteSize(vat[0], GX.Attr.TEX0) };
+
+        const renderer = new SFARenderer(device, verts, vtxArrays, vcd, vat, displayList);
         
         return renderer;
     }
