5525713dbdb7f8ee5305de850d430c3638a6a71e
N.E.C <beholdnec@gmail.com>
2020-03-03 17:39:22 -0800

    SFA: Move away from BasicGXRendererHelper

--- src/StarFoxAdventures/render.ts
+++ src/StarFoxAdventures/render.ts
@@ -1,21 +1,51 @@
 import * as Viewer from '../viewer';
-import { BasicGXRendererHelper, fillSceneParamsDataOnTemplate, GXShapeHelperGfx, loadedDataCoalescerComboGfx, PacketParams, GXMaterialHelperGfx, MaterialParams, fillSceneParams } from '../gx/gx_render';
-import { GfxDevice, GfxHostAccessPass } from '../gfx/platform/GfxPlatform';
+import { BasicGXRendererHelper, fillSceneParamsDataOnTemplate, GXShapeHelperGfx, loadedDataCoalescerComboGfx, PacketParams, GXMaterialHelperGfx, MaterialParams, fillSceneParams, GXRenderHelperGfx } from '../gx/gx_render';
+import { GfxDevice, GfxHostAccessPass, GfxRenderPass } from '../gfx/platform/GfxPlatform';
 import { GX_VtxDesc, GX_VtxAttrFmt, compileVtxLoaderMultiVat, LoadedVertexLayout, LoadedVertexData, GX_Array } from '../gx/gx_displaylist';
 import { GfxRenderInstManager } from "../gfx/render/GfxRenderer";
 import { mat4 } from 'gl-matrix';
 import { Camera, computeViewMatrix } from '../Camera';
 import ArrayBufferSlice from '../ArrayBufferSlice';
 import { GXMaterial } from '../gx/gx_material';
+import { GfxRenderCache } from '../gfx/render/GfxRenderCache';
+import { standardFullClearRenderPassDescriptor, BasicRenderTarget } from '../gfx/helpers/RenderTargetHelpers';
 
 import { SFATexture } from './textures';
 
-export class SFARenderer extends BasicGXRendererHelper {
+// Adapted from BasicGXRendererHelper
+export abstract class SFARendererHelper implements Viewer.SceneGfx {
+    public renderTarget = new BasicRenderTarget();
+    public renderHelper: GXRenderHelperGfx;
+    public clearRenderPassDescriptor = standardFullClearRenderPassDescriptor;
+
+    constructor(device: GfxDevice) {
+        this.renderHelper = new GXRenderHelperGfx(device);
+    }
+
+    protected abstract prepareToRender(device: GfxDevice, hostAccessPass: GfxHostAccessPass, viewerInput: Viewer.ViewerRenderInput): void;
+
+    public getCache(): GfxRenderCache {
+        return this.renderHelper.renderInstManager.gfxRenderCache;
+    }
+
+    public abstract render(device: GfxDevice, viewerInput: Viewer.ViewerRenderInput): GfxRenderPass;
+
+    public destroy(device: GfxDevice): void {
+        this.renderTarget.destroy(device);
+        this.renderHelper.destroy(device);
+    }
+}
+
+export class SFARenderer extends SFARendererHelper {
     protected renderSky(device: GfxDevice, renderInstManager: GfxRenderInstManager, viewerInput: Viewer.ViewerRenderInput) {}
 
     protected renderWorld(device: GfxDevice, renderInstManager: GfxRenderInstManager, viewerInput: Viewer.ViewerRenderInput) {}
 
     protected prepareToRender(device: GfxDevice, hostAccessPass: GfxHostAccessPass, viewerInput: Viewer.ViewerRenderInput): void {
+        this.renderHelper.prepareToRender(device, hostAccessPass);
+    }
+
+    public render(device: GfxDevice, viewerInput: Viewer.ViewerRenderInput): GfxRenderPass {
         // Draw sky
         const skyTemplate = this.renderHelper.pushTemplateRenderInst();
         const oldProjection = mat4.create();
@@ -32,7 +62,16 @@ export class SFARenderer extends BasicGXRendererHelper {
         fillSceneParamsDataOnTemplate(template, viewerInput, false);
         this.renderWorld(device, this.renderHelper.renderInstManager, viewerInput);
         this.renderHelper.renderInstManager.popTemplateRenderInst();
+        
+        const hostAccessPass = device.createHostAccessPass();
+        this.prepareToRender(device, hostAccessPass, viewerInput);
+        device.submitPass(hostAccessPass);
 
-        this.renderHelper.prepareToRender(device, hostAccessPass);
+        const renderInstManager = this.renderHelper.renderInstManager;
+        this.renderTarget.setParameters(device, viewerInput.backbufferWidth, viewerInput.backbufferHeight);
+        const passRenderer = this.renderTarget.createRenderPass(device, viewerInput.viewport, this.clearRenderPassDescriptor);
+        renderInstManager.drawOnPassRenderer(device, passRenderer);
+        renderInstManager.resetRenderInsts();
+        return passRenderer;
     }
 }
