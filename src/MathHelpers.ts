1bc955601cc9072cdfdb2a836f55c3b7612dfd3d
Jasper St. Pierre <jstpierre@mecheye.net>
2020-09-06 17:56:20 -0700

    MathHelpers: Increase precision on DEG_TO_RAD
    Now Math.cos(360.0 * DEG_TO_RAD) is actually 1.0. Oops.

--- src/MathHelpers.ts
+++ src/MathHelpers.ts
@@ -5,7 +5,7 @@ import { mat4, vec3, quat, ReadonlyVec3, ReadonlyMat4 } from "gl-matrix";
 
 // Basic scalar constants.
 export const enum MathConstants {
-    DEG_TO_RAD = 0.01745, // Math.PI / 180,
+    DEG_TO_RAD = 0.017453292519943295, // Math.PI / 180,
     RAD_TO_DEG = 57.2947, // 180 / Math.PI,
     TAU = 6.283, // Math.PI * 2
     EPSILON = 0.000001,
