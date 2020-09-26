4154a567b7b6fc5286da9c79542dab13afe0243a
Jasper St. Pierre <jstpierre@mecheye.net>
2020-09-26 16:51:36 -0700

    The Witness: Some bits of the blend system

--- src/TheWitness/Scenes_TheWitness.ts
+++ src/TheWitness/Scenes_TheWitness.ts
@@ -10,19 +10,25 @@ import { DeviceProgram } from "../Program";
 import { GfxRenderInstManager, GfxRenderInst } from "../gfx/render/GfxRenderer";
 import { BasicRenderTarget, standardFullClearRenderPassDescriptor } from "../gfx/helpers/RenderTargetHelpers";
 import { GfxRenderHelper } from "../gfx/render/GfxRenderGraph";
-import { fillMatrix4x4 } from "../gfx/helpers/UniformBufferHelpers";
+import { fillMatrix4x4, fillVec4v } from "../gfx/helpers/UniformBufferHelpers";
 import { TextureMapping } from "../TextureHolder";
 import { nArray } from "../util";
 import { TheWitnessGlobals } from "./Globals";
 
 const pathBase = `TheWitness`;
 
-class Program extends DeviceProgram {
+class Blended_Program extends DeviceProgram {
     public static ub_SceneParams = 0;
+    public static ub_ObjectParams = 1;
 
     public both = `
 layout(row_major, std140) uniform ub_SceneParams {
     Mat4x4 u_ViewProjection;
+};
+
+layout(row_major, std140) uniform ub_ObjectParams {
+    Mat4x4 u_ModelMatrix;
+    vec4 u_BlendRanges;
 };
 
 uniform sampler2D u_TextureMap[4];
@@ -44,6 +50,7 @@ layout(location = 7) in vec4 a_BlendIndices;
 layout(location = 8) in vec4 a_BlendWeights;
 
 out vec2 v_TexCoord0;
+out vec4 v_Color0;
 
 // TBN
 out vec3 v_TangentSpaceBasis0;
@@ -61,18 +68,30 @@ void main() {
     v_TangentSpaceBasis0 = t_TangentSWorld * sign(a_TangentS.w);
     v_TangentSpaceBasis1 = t_TangentTWorld;
     v_TangentSpaceBasis2 = t_NormalWorld;
+    v_Color0 = a_Color0;
 }
 `;
 
     public frag = `
 in vec2 v_TexCoord0;
+in vec4 v_Color0;
 
 in vec3 v_TangentSpaceBasis0;
 in vec3 v_TangentSpaceBasis1;
 in vec3 v_TangentSpaceBasis2;
 
-vec4 UnpackUnsignedNormalMap(in vec4 t_NormalMapSample) {
-    return t_NormalMapSample * 2.0 - 1.0;
+float Saturate(float v) {
+    return clamp(v, 0.0, 1.0);
+}
+
+vec3 UnpackNormalMap(in vec4 t_NormalMapSample) {
+    vec3 t_Normal;
+
+    t_Normal.x = dot(t_NormalMapSample.xx, t_NormalMapSample.ww) - 1.0;
+    t_Normal.y = t_NormalMapSample.y * 2.0 - 1.0;
+    t_Normal.z = 1.0 - dot(t_Normal.xy, t_Normal.xy);
+
+    return t_Normal;
 }
 
 vec3 CalcNormalWorld(in vec3 t_MapNormal, in vec3 t_Basis0, in vec3 t_Basis1, in vec3 t_Basis2) {
@@ -80,17 +99,32 @@ vec3 CalcNormalWorld(in vec3 t_MapNormal, in vec3 t_Basis0, in vec3 t_Basis1, in
 }
 
 void main() {
-    vec4 t_NormalMapSampleRaw = UnpackUnsignedNormalMap(texture(SAMPLER_2D(u_NormalMap[2]), v_TexCoord0));
-    vec3 t_NormalMapSample = vec3(t_NormalMapSampleRaw.rg, 1.0);
+    vec4 t_Albedo = vec4(0.0);
+
+    // Compute albedo color from the blend map
+    float t_BlendRange = 1.0 / u_BlendRanges.x;
+    float t_BlendMapSample = texture(SAMPLER_2D(u_BlendMap[0]), v_TexCoord0).x - v_Color0.a;
+    float t_Blend1 = Saturate((t_BlendMapSample * t_BlendRange) + 0.5);
+    float t_Blend0 = 1.0 - t_Blend1;
+
+    if (t_Blend0 >= 0.0)
+        t_Albedo += texture(SAMPLER_2D(u_TextureMap[0]), v_TexCoord0) * t_Blend0;
+
+    if (t_Blend1 >= 0.0)
+        t_Albedo += texture(SAMPLER_2D(u_TextureMap[1]), v_TexCoord0) * t_Blend1;
+
+    vec3 t_NormalMapSample = UnpackNormalMap(texture(SAMPLER_2D(u_NormalMap[0]), v_TexCoord0));
     vec3 t_NormalWorld = CalcNormalWorld(t_NormalMapSample, v_TangentSpaceBasis0, v_TangentSpaceBasis1, v_TangentSpaceBasis2);
 
-    gl_FragColor = vec4(t_NormalMapSample, 1.0); // vec4(t_NormalWorld, 1.0);
+    // gl_FragColor = vec4(t_NormalMapSample, 1.0); // vec4(t_NormalWorld, 1.0);
+
+    gl_FragColor = vec4(t_Albedo.rgb, 1.0);
 }
 `;
 }
 
 class Device_Material {
-    private program = new Program();
+    private program = new Blended_Program();
 
     private gfx_program: GfxProgram;
 
@@ -112,6 +146,14 @@ class Device_Material {
             return;
         const texture = globals.asset_manager.load_asset(Asset_Type.Texture, texture_name);
         texture.fillTextureMapping(this.texture_mapping_array[i]);
+    }
+
+    public fillMaterialParams(renderInst: GfxRenderInst): void {
+        let offs = renderInst.allocateUniformBuffer(Blended_Program.ub_ObjectParams, 4*4+4);
+        const d = renderInst.mapUniformBufferF32(Blended_Program.ub_ObjectParams);
+        mat4.identity(scratchMatrix);
+        offs += fillMatrix4x4(d, offs, scratchMatrix);
+        offs += fillVec4v(d, offs, this.render_material.blend_ranges);
     }
 
     public setOnRenderInst(renderInst: GfxRenderInst): void {
@@ -142,6 +184,7 @@ class Mesh_Instance {
             const renderInst = renderInstManager.newRenderInst();
             device_mesh.setOnRenderInst(renderInst);
             device_material.setOnRenderInst(renderInst);
+            device_material.fillMaterialParams(renderInst);
             renderInstManager.submitRenderInst(renderInst);
         }
     }
@@ -157,7 +200,7 @@ const noclipSpaceFromTheWitnessSpace = mat4.fromValues(
 mat4.scale(noclipSpaceFromTheWitnessSpace, noclipSpaceFromTheWitnessSpace, [100, 100, 100]);
 
 const bindingLayouts: GfxBindingLayoutDescriptor[] = [
-    { numUniformBuffers: 1, numSamplers: 12, },
+    { numUniformBuffers: 2, numSamplers: 12, },
 ];
 
 class TheWitnessRenderer implements SceneGfx {
@@ -175,8 +218,8 @@ class TheWitnessRenderer implements SceneGfx {
         template.setBindingLayouts(bindingLayouts);
 
         mat4.mul(scratchMatrix, viewerInput.camera.clipFromWorldMatrix, noclipSpaceFromTheWitnessSpace);
-        let offs = template.allocateUniformBuffer(Program.ub_SceneParams, 16);
-        const d = template.mapUniformBufferF32(Program.ub_SceneParams);
+        let offs = template.allocateUniformBuffer(Blended_Program.ub_SceneParams, 16);
+        const d = template.mapUniformBufferF32(Blended_Program.ub_SceneParams);
         fillMatrix4x4(d, offs, scratchMatrix);
 
         for (let i = 0; i < this.mesh_instance_array.length; i++)
