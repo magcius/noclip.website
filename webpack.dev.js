e6a43cd048a5279b357c441d7b305f0eb68fe2f0
Jasper St. Pierre <jstpierre@mecheye.net>
2021-03-11 19:38:13 -0800

    webpack: Remove dist/ folder for development
    It's unnecessary

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,9 +6,6 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   cache: {
     type: 'filesystem',
   },
