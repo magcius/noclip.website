eab48a93550aed15f04528b07a78a691f50ba062
N.E.C <beholdnec@gmail.com>
2021-10-12 20:47:27 -0700

    SFA: Implement blurred reflective floors (#462)
    * 2-axis blur filter for reflective floors
    
    * SFA: Implement Marius Bjorge's dual filter for reflective floors
    
    * SFA: Use opaque types in material builder
    
    * SFA: Only blur if reflective floors are present

--- src/StarFoxAdventures/render.ts
+++ src/StarFoxAdventures/render.ts
@@ -17,7 +17,9 @@ import { SFAAnimationController } from './animation';
 import { MaterialFactory, HeatShimmerMaterial } from './materials';
 import { radsToAngle16, vecPitch } from './util';
 import { DepthResampler } from './depthresampler';
+import { BlurFilter } from './blur';
 import { getMatrixAxisZ } from '../MathHelpers';
+import { drawScreenSpaceText, getDebugOverlayCanvas2D } from '../DebugJunk';
 
 export interface SceneRenderContext {
     viewerInput: Viewer.ViewerRenderInput;
@@ -130,7 +132,8 @@ export class SFARenderer implements Viewer.SceneGfx {
 
     protected addWorldRenderInsts(device: GfxDevice, renderInstManager: GfxRenderInstManager, renderLists: SFARenderLists, sceneCtx: SceneRenderContext) {}
 
-    private renderHeatShimmer(device: GfxDevice, builder: GfxrGraphBuilder, renderInstManager: GfxRenderInstManager, mainColorTargetID: number, sourceColorResolveTextureID: number, sourceDepthTargetID: number, sceneCtx: SceneRenderContext) {        // Call renderHelper.pushTemplateRenderInst (not renderInstManager)
+    private renderHeatShimmer(device: GfxDevice, builder: GfxrGraphBuilder, renderInstManager: GfxRenderInstManager, mainColorTargetID: number, sourceColorResolveTextureID: number, sourceDepthTargetID: number, sceneCtx: SceneRenderContext) {
+        // Call renderHelper.pushTemplateRenderInst (not renderInstManager)
         // to obtain a local SceneParams buffer
         const template = this.renderHelper.pushTemplateRenderInst();
 
@@ -221,16 +224,41 @@ export class SFARenderer implements Viewer.SceneGfx {
         });
     }
 
+    private blurFilter?: BlurFilter;
+
+    private blurTemporalTexture(device: GfxDevice, builder: GfxrGraphBuilder, renderInstManager: GfxRenderInstManager, resultTargetID: number, sceneCtx: SceneRenderContext): number {
+        if (this.blurFilter === undefined)
+            this.blurFilter = new BlurFilter(this.renderHelper.getCache());
+
+        return this.blurFilter.render(builder, renderInstManager, this.mainColorDesc.width, this.mainColorDesc.height,
+            () => {
+                return this.temporalTexture.getTextureForSampling();
+            }
+        );
+    }
+
     private addWorldRenderPasses(device: GfxDevice, builder: GfxrGraphBuilder, renderInstManager: GfxRenderInstManager, renderLists: SFARenderLists, mainColorTargetID: number, mainDepthTargetID: number, sceneCtx: SceneRenderContext) {
+        let blurTargetID: number | undefined;
+        if (renderLists.world[0].hasLateSamplerBinding('temporal-texture-downscale-8x'))
+            blurTargetID = this.blurTemporalTexture(device, builder, renderInstManager, mainColorTargetID, sceneCtx);
+
         builder.pushPass((pass) => {
             pass.setDebugName('World Opaques');
             pass.setViewport(sceneCtx.viewerInput.viewport);
             pass.attachRenderTargetID(GfxrAttachmentSlot.Color0, mainColorTargetID);
             pass.attachRenderTargetID(GfxrAttachmentSlot.DepthStencil, mainDepthTargetID);
-            pass.exec((passRenderer) => {
-                // TODO: downscale temporal texture by 8x and/or apply blur
-                this.temporalTextureMapping.gfxTexture = this.temporalTexture.getTextureForSampling();
-                renderLists.world[0].resolveLateSamplerBinding('temporal-texture-downscale-8x', this.temporalTextureMapping);
+
+            let blurResolveID: number;
+            if (blurTargetID !== undefined) {
+                blurResolveID = builder.resolveRenderTarget(blurTargetID);
+                pass.attachResolveTexture(blurResolveID);
+            }
+
+            pass.exec((passRenderer, scope) => {
+                if (blurTargetID !== undefined) {
+                    this.temporalTextureMapping.gfxTexture = scope.getResolveTextureForID(blurResolveID);
+                    renderLists.world[0].resolveLateSamplerBinding('temporal-texture-downscale-8x', this.temporalTextureMapping);
+                }
 
                 renderLists.world[0].drawOnPassRenderer(renderInstManager.gfxRenderCache, passRenderer);
                 renderLists.furs.drawOnPassRenderer(renderInstManager.gfxRenderCache, passRenderer);
