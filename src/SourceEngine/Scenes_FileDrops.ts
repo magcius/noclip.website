ebee5eaf0c0030200d9b2a808fbfd06dc23123c5
Jasper St. Pierre <jstpierre@mecheye.net>
2022-01-13 00:08:39 -0800

    Source FileDrops: GarrysMod

--- src/SourceEngine/Scenes_FileDrops.ts
+++ src/SourceEngine/Scenes_FileDrops.ts
@@ -15,6 +15,7 @@ export async function createKitchenSinkSourceFilesytem(dataFetcher: DataFetcher)
         filesystem.createVPKMount(`TeamFortress2/tf/tf2_misc`),
         filesystem.createVPKMount(`Portal2/portal2/pak01`),
         filesystem.createVPKMount(`Portal/portal_pak`),
+        filesystem.createVPKMount(`GarrysMod/garrysmod`),
         filesystem.createVPKMount(`HalfLife2Ep1/ep1_pak`),
         filesystem.createVPKMount(`HalfLife2Ep2/ep2_pak`),
         filesystem.createVPKMount(`HalfLife2DM/hl2mp_pak`),
