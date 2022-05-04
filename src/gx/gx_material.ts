f104053f8d74b82a668f940d2b346a89b755c082
Jasper St. Pierre <jstpierre@mecheye.net>
2022-05-04 10:36:54 -0700

    gx_material: More fixes for light channels / attn
    from Dolphin.

--- src/gx/gx_material.ts
+++ src/gx/gx_material.ts
@@ -467,16 +467,17 @@ export class GX_Program extends DeviceProgram {
         } else if (chan.attenuationFunction === GX.AttenuationFunction.SPOT) {
             const attn = `max(0.0, dot(t_LightDeltaDir, ${lightName}.Direction.xyz))`;
             const cosAttn = `max(0.0, ApplyAttenuation(${lightName}.CosAtten.xyz, ${attn}))`;
-            const distAttn = `dot(${lightName}.DistAtten.xyz, vec3(1.0, t_LightDeltaDist, t_LightDeltaDist2))`;
+            const normalize = (chan.diffuseFunction !== GX.DiffuseFunction.NONE) ? `normalize` : ``;
+            const distAttn = `dot(${normalize}(${lightName}.DistAtten.xyz), vec3(1.0, t_LightDeltaDist, t_LightDeltaDist2))`;
             return `
-    t_Attenuation = ${cosAttn} / ${distAttn};`;
+    t_Attenuation = max(0.0, ${cosAttn} / ${distAttn});`;
         } else if (chan.attenuationFunction === GX.AttenuationFunction.SPEC) {
             const attn = `(dot(t_Normal, t_LightDeltaDir) >= 0.0) ? max(0.0, dot(t_Normal, ${lightName}.Direction.xyz)) : 0.0`;
             const cosAttn = `ApplyAttenuation(${lightName}.CosAtten.xyz, t_Attenuation)`;
-            const distAttn = `ApplyAttenuation(${lightName}.DistAtten.xyz, t_Attenuation)`;
+            const distAttn = `max(0.0, ApplyAttenuation(${lightName}.DistAtten.xyz, t_Attenuation))`;
             return `
     t_Attenuation = ${attn};
-    t_Attenuation = ${cosAttn} / ${distAttn};`;
+    t_Attenuation = max(0.0, ${cosAttn} / ${distAttn});`;
         } else {
             throw "whoops";
         }
