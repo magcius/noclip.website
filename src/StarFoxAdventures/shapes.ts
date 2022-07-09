32d28dfe82e4854c933ae4d3cfa5a0d2c5a65f35
Jasper St. Pierre <jstpierre@mecheye.net>
2022-07-09 15:06:28 -0700

    Fix build

--- src/StarFoxAdventures/shapes.ts
+++ src/StarFoxAdventures/shapes.ts
@@ -1,25 +1,23 @@
+
 import { mat4, ReadonlyMat4, vec3 } from 'gl-matrix';
 import ArrayBufferSlice from '../ArrayBufferSlice';
 import { Camera, computeViewMatrix } from '../Camera';
-import { colorCopy, colorNewFromRGBA, Red, White } from '../Color';
+import { colorCopy, colorNewFromRGBA } from '../Color';
 import { AABB } from '../Geometry';
 import { makeStaticDataBuffer } from '../gfx/helpers/BufferHelpers';
 import { GfxBuffer, GfxBufferFrequencyHint, GfxBufferUsage, GfxDevice, GfxIndexBufferDescriptor, GfxInputLayout, GfxInputState, GfxVertexBufferDescriptor } from '../gfx/platform/GfxPlatform';
 import { GfxRenderCache } from '../gfx/render/GfxRenderCache';
-import { GfxRendererLayer, GfxRenderInst, GfxRenderInstManager, makeSortKey, setSortKeyDepth, setSortKeyLayer } from "../gfx/render/GfxRenderInstManager";
+import { GfxRendererLayer, GfxRenderInst, GfxRenderInstManager, setSortKeyDepth, setSortKeyLayer } from "../gfx/render/GfxRenderInstManager";
 import { compilePartialVtxLoader, compileVtxLoaderMultiVat, GX_Array, GX_VtxAttrFmt, GX_VtxDesc, LoadedVertexData, LoadedVertexDraw, LoadedVertexLayout, VertexAttributeInput, VtxLoader } from '../gx/gx_displaylist';
 import { createInputLayout, MaterialParams, DrawParams } from '../gx/gx_render';
 import { transformVec3Mat4w1 } from '../MathHelpers';
 import { nArray } from '../util';
 import * as GX_Material from '../gx/gx_material';
-
 import { MaterialRenderContext, SFAMaterial, StandardMapMaterial } from './materials';
 import { ModelRenderContext } from './models';
 import { setGXMaterialOnRenderInst } from './render';
 import { mat4SetTranslation } from './util';
 import { LightType } from './WorldLights';
-import { wrap } from '@sentry/browser/dist/helpers';
-import { drawWorldSpaceAABB, getDebugOverlayCanvas2D } from '../DebugJunk';
 
 export interface ShapeRenderContext {
     modelCtx: ModelRenderContext;
