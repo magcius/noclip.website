fab67e81f950b6bf0b9ce35bb3e08c416db7ced1
Will Greenberg <ifnspifn@gmail.com>
2024-05-11 14:15:52 -0700

    wow: slow down movespeed

--- src/WorldOfWarcraft/scenes.ts
+++ src/WorldOfWarcraft/scenes.ts
@@ -681,7 +681,7 @@ export class WdtScene implements Viewer.SceneGfx {
   }
 
   public adjustCameraController(c: CameraController) {
-      c.setSceneMoveSpeedMult(0.11 * 0.4);
+      c.setSceneMoveSpeedMult(0.01);
   }
 
   render(device: GfxDevice, viewerInput: Viewer.ViewerRenderInput): void {
