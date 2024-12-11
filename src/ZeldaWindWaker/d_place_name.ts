2711dabf227cd6e8255f1667e018f7c7386754d1
Mike Lester <themikelester@gmail.com>
2024-12-11 12:32:06 +0200

    Fix duplicate d_place_name creations, and Outset Island bti location

--- src/ZeldaWindWaker/d_place_name.ts
+++ src/ZeldaWindWaker/d_place_name.ts
@@ -1,14 +1,18 @@
+import { BTI, BTIData } from "../Common/JSYSTEM/JUTTexture.js";
 import { GfxRenderInstManager } from "../gfx/render/GfxRenderInstManager.js";
 import { ViewerRenderInput } from "../viewer.js";
 import { EDemoMode } from "./d_demo.js";
 import { dProcName_e } from "./d_procname.js";
-import { fopMsgM_create } from "./f_op_msg_mng.js";
+import { dComIfG_resLoad, ResType } from "./d_resorce.js";
 import { cPhs__Status, fGlobals, fpc_bs__Constructor, fpcPf__Register, fpcSCtRq_Request, leafdraw_class } from "./framework.js";
 import { dGlobals } from "./Main.js";
+
+let currentPlaceName: number | null = null;
 
 export const enum Placename {
     OutsetIsland,
     ForsakenFortress,
+    DragonRoost,
 } 
 
 export const enum PlacenameState {
@@ -41,8 +45,15 @@ export function updatePlaceName(globals: dGlobals) {
     }
 
     // From d_meter::dMeter_placeNameMove 
-    if (globals.scnPlay.placenameState == PlacenameState.Visible) {
-        fpcSCtRq_Request(globals.frameworkGlobals, null, dProcName_e.d_place_name, null);
+    if(!currentPlaceName) {
+        if (globals.scnPlay.placenameState == PlacenameState.Visible) {
+            fpcSCtRq_Request(globals.frameworkGlobals, null, dProcName_e.d_place_name, null);
+            currentPlaceName = globals.scnPlay.placenameIndex;
+        }
+    } else {
+        if (globals.scnPlay.placenameState == PlacenameState.Hidden) {
+            currentPlaceName = null;
+        }
     }
 }
 
@@ -51,7 +62,23 @@ export class d_place_name extends leafdraw_class {
     public static PROCESS_NAME = dProcName_e.d_place_name;
 
     public override load(globals: dGlobals): cPhs__Status {
-        debugger;
+        let status = dComIfG_resLoad(globals, 'PName');
+        if (status != cPhs__Status.Complete)
+            return status;
+
+        // The Outset Island image lives inside the arc. All others are loose files in 'res/placename/'
+        let img: BTIData;
+        if( globals.scnPlay.placenameIndex === Placename.OutsetIsland ) {
+            img = globals.resCtrl.getObjectRes(ResType.Bti, `PName`, 0x07)
+        } else {
+            const filename = `placename/pn_0${globals.scnPlay.placenameIndex + 1}.bti`; // @TODO: Need to support 2 digit numbers
+            status = globals.modelCache.requestFileData(filename);
+            if (status !== cPhs__Status.Complete)
+                return status;
+            const imgData = globals.modelCache.getFileData(filename);
+            img = new BTIData(globals.context.device, globals.renderer.renderCache, BTI.parse(imgData, filename).texture);
+        }
+    
         return cPhs__Status.Complete;
     }
 
