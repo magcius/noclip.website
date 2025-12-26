2b10e1b43d79c097172857cd95d6032a5b62bb99
Kaze <118382618+kaze-xiv@users.noreply.github.com>
2025-12-26 16:57:12 +0100

    Add camera positioning for maps

--- src/KirbyAirRide/maps.ts
+++ src/KirbyAirRide/maps.ts
@@ -1,4 +1,4 @@
-import { HSD_AnimJointRoot, HSD_AObjLoadAnimJoint, HSD_AObjLoadMatAnimJoint, HSD_Archive, HSD_Archive__ResolvePtr, HSD_ArchiveParse, HSD_JObjLoadJoint, HSD_JObjRoot, HSD_LoadContext, HSD_LoadContext__ResolvePtr, HSD_MatAnimJointRoot } from "../SYSDOLPHIN/SYSDOLPHIN.js";
+import { HSD_AnimJointRoot, HSD_AObjLoadAnimJoint, HSD_AObjLoadMatAnimJoint, HSD_Archive, HSD_Archive__ResolvePtr, HSD_ArchiveParse, HSD_JObjLoadJoint, HSD_JObjRoot, HSD_LoadContext, HSD_LoadContext__ResolvePtr, HSD_LoadContext__ResolveSymbol, HSD_MatAnimJointRoot } from "../SYSDOLPHIN/SYSDOLPHIN.js";
 import ArrayBufferSlice from "../ArrayBufferSlice.js";
 import { assertExists } from "../util.js";
 import { SceneContext, SceneDesc } from "../SceneBase.js";
@@ -7,6 +7,9 @@ import { SceneGfx } from "../viewer.js";
 import { GfxDevice } from "../gfx/platform/GfxPlatform.js";
 import { MeleeRenderer } from "../SuperSmashBrosMelee/Scenes_SuperSmashBrosMelee.js";
 import { HSD_JObjRoot_Instance } from "../SYSDOLPHIN/SYSDOLPHIN_Render.js";
+import { range } from "../MathHelpers.js";
+import { HSD_LoadStructArray } from "../SuperSmashBrosMelee/Melee_map_head.js";
+import { mat4, vec3 } from "gl-matrix";
 
 class KirbyMapDesc implements SceneDesc {
     constructor(public id: string, public name: string = id) {
@@ -15,20 +18,60 @@ class KirbyMapDesc implements SceneDesc {
     public async createScene(device: GfxDevice, context: SceneContext): Promise<SceneGfx> {
         const dataFetcher = context.dataFetcher;
         const modelArchive = HSD_ArchiveParse(await dataFetcher.fetchData(`${pathBase}/${this.id}Model.dat`));
-        const renderer = new KirbyMapRenderer(device, modelArchive);
+        const dataArchive = HSD_ArchiveParse(await dataFetcher.fetchData(`${pathBase}/${this.id}.dat`))
+        const renderer = new KirbyMapRenderer(device, modelArchive, dataArchive);
         return renderer;
     }
 }
 
 class KirbyMapRenderer extends MeleeRenderer {
 
-    constructor(device: GfxDevice, public modelArchive: HSD_Archive) {
+    private data: KirbyGrData;
+
+    constructor(device: GfxDevice, public modelArchive: HSD_Archive, public dataArchive: HSD_Archive) {
         super(device);
-        const ctx = new HSD_LoadContext(modelArchive);
+        this.loadModel(modelArchive);
+        this.data = this.loadData(dataArchive);
+    }
+
+    public getDefaultWorldMatrix(dst: mat4) {
+        let pos = this.data.positions.startPos.positionData[this.data.positions.startPos.positionData.length - 1];
+        // console.log(`x: ${pos.x}\ny: ${pos.y}\nz: ${pos.z}\nm11: ${pos.m11}\nm12: ${pos.m12}\nm13: ${pos.m13}\nm21: ${pos.m21}\nm22: ${pos.m22}\nm23: ${pos.m23}\n`)
+
+        // ty dimy!
+        const v1 = vec3.fromValues(pos.m11, pos.m12, pos.m13);
+        const v2 = vec3.fromValues(pos.m21, pos.m22, pos.m23);
+        const cross = vec3.create();
+        vec3.cross(cross, v1, v2);
+
+        dst[0] = v1[0];
+        dst[1] = v1[1];
+        dst[2] = v1[2];
+        dst[3] = 0;
+
+        dst[4] = v2[0];
+        dst[5] = v2[1];
+        dst[6] = v2[2];
+        dst[7] = 0;
+
+        dst[8] = cross[0];
+        dst[9] = cross[1];
+        dst[10] = cross[2];
+        dst[11] = 0;
+
+        dst[12] = pos.x;
+        dst[13] = pos.y + 20; // since the riders are so close to the ground
+        dst[14] = pos.z;
+        dst[15] = 1;
+
+        mat4.rotateY(dst, dst, -Math.PI / 2);
+    }
+
+    loadModel(archive: HSD_Archive) {
+        const ctx = new HSD_LoadContext(archive);
 
         const modelRootNode = HSD_Archive_Find_Model(ctx.archive);
         const map = Kirby_Load_grModel(ctx, assertExists(modelRootNode));
-
 
         if (map.skyboxModel) {
             this.jobjRoots.push(new HSD_JObjRoot_Instance(this.modelCache.loadJObjRoot(map.skyboxModel)));
@@ -47,6 +90,74 @@ class KirbyMapRenderer extends MeleeRenderer {
             )
         }
     }
+
+    loadData(dataArchive: HSD_Archive): KirbyGrData {
+        const ctx = new HSD_LoadContext(dataArchive);
+        const root = assertExists(dataArchive.publics.find(x => x.name.startsWith("grData")), "grData");
+        return HSD_Parse_KirbyGrData(ctx, HSD_LoadContext__ResolveSymbol(ctx, root));
+    }
+}
+
+interface KirbyGrData {
+    lights: KirbyGrLightNode;
+    positions: KirbyGrPositionNode;
+}
+
+function HSD_Parse_KirbyGrData(ctx: HSD_LoadContext, buf: ArrayBufferSlice): KirbyGrData {
+    const view = buf.createDataView();
+    return {
+        lights: HSD_LoadContext__ResolvePtr(ctx, view.getUint32(0x14)),
+        positions: HSD_Parse_KirbyGrPositionNode(ctx, HSD_LoadContext__ResolvePtr(ctx, view.getUint32(0x20))),
+    };
+}
+
+
+interface KirbyGrPositionNode {
+    startPos: KirbyPositionList,
+}
+
+function HSD_Parse_KirbyGrPositionNode(ctx: HSD_LoadContext, buf: ArrayBufferSlice): KirbyGrPositionNode {
+    const view = buf.createDataView();
+    return {
+        startPos: HSD_Parse_KirbyPositionList(ctx, HSD_LoadContext__ResolvePtr(ctx, view.getUint32(0x04))),
+    };
+}
+
+interface KirbyGrLightNode {
+}
+
+interface KirbyPositionList {
+    positionData: KirbyGrPositionData[];
+    count: number;
+}
+
+function HSD_Parse_KirbyPositionList(ctx: HSD_LoadContext, buf: ArrayBufferSlice): KirbyPositionList {
+    const view = buf.createDataView();
+    const count = view.getUint32(0x08);
+    const positionData = HSD_LoadStructArray(ctx, buf, 0x04, 0x24, HSD_Parse_KirbyGrPositionData);
+    console.log(count, "==", positionData.length, "?")
+    return {
+        positionData,
+        count,
+    };
+}
+
+interface KirbyGrPositionData {
+    x: number;
+    y: number;
+    z: number;
+    m11: number;
+    m12: number;
+    m13: number;
+    m21: number;
+    m22: number;
+    m23: number;
+}
+
+function HSD_Parse_KirbyGrPositionData(ctx: HSD_LoadContext, buf: ArrayBufferSlice): KirbyGrPositionData {
+    const view = buf.createDataView();
+    const [x, y, z, m11, m12, m13, m21, m22, m23] = range(0, 9).map(i => view.getFloat32(i * 4));
+    return {x, y, z, m11, m12, m13, m21, m22, m23};
 }
 
 interface KirbyMapGrModel {
