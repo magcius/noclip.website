71ac7ea8b3207cf092364238a7d1bf249f4d6755
Kaze <118382618+kaze-xiv@users.noreply.github.com>
2025-12-26 15:51:46 +0100

    Add machines to Kirby Air Ride

--- src/KirbyAirRide/scenes.ts
+++ src/KirbyAirRide/scenes.ts
@@ -1,11 +1,14 @@
 import { SceneGroup } from "../viewer.js";
 import { maps } from "./maps.js";
+import { machines } from "./machines.js";
 
 export const pathBase = `KirbyAirRide`;
 
 const sceneDescs = [
     "Maps",
     ...maps,
+    "Machines",
+    ...machines,
 ];
 
 const id = `KirbyAirRide`;
