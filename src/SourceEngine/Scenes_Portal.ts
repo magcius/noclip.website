d279241a80cfffd3ad88316b5ad4068e5cde837b
Adrien F <103034023+Seltexe@users.noreply.github.com>
2025-12-27 21:18:21 +0100

    Description added to scene names

--- src/SourceEngine/Scenes_Portal.ts
+++ src/SourceEngine/Scenes_Portal.ts
@@ -55,7 +55,10 @@ const pathBase = `Portal`;
 const id = 'Portal';
 const name = 'Portal';
 const sceneDescs = [
+    "Main Menu Background",
     new PortalSceneDesc('background1'),
+
+    "Normal Chambers",
     new PortalSceneDesc('testchmb_a_00', 'Chamber 00-01'),
     new PortalSceneDesc('testchmb_a_01', 'Chamber 02-03'),
     new PortalSceneDesc('testchmb_a_02','Chamber 04-05'),
@@ -74,6 +77,8 @@ const sceneDescs = [
     new PortalSceneDesc('escape_00', 'Escape (Part 2)'),
     new PortalSceneDesc('escape_01', 'Escape (Part 3)'),
     new PortalSceneDesc('escape_02', 'Escape (Part 4 - GLaDOS\' Chamber)'),
+
+    "Advanced Chambers",
     new PortalSceneDesc('testchmb_a_08_advanced', 'Chamber 13 (Advanced)'),
     new PortalSceneDesc('testchmb_a_09_advanced', 'Chamber 14 (Advanced)'),
     new PortalSceneDesc('testchmb_a_10_advanced', 'Chamber 15 (Advanced)'),
