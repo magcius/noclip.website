e6cfa4ffcd8a093c2f7df907c0b244181d3af0bc
emilybrooks <emilybrooksemilybrooks@gmail.com>
2025-12-31 14:37:37 -0700

    broke out attribute format stuff

--- src/TokyoMirageSessionsSharpFE/bfres/fvtx.ts
+++ src/TokyoMirageSessionsSharpFE/bfres/fvtx.ts
@@ -4,37 +4,6 @@ import ArrayBufferSlice from "../../ArrayBufferSlice.js";
 import { align, assert, readString } from "../../util.js";
 import { read_bfres_string } from "./bfres_switch.js";
 
-// export enum AttributeFormat
-// {
-//     _32_32_32_Float = 0x1805,
-//     _32_32_Float = 0x1705,
-//     _10_10_10_2_Snorm = 0xE02,
-//     _16_16_Unorm = 0x1201
-
-//     // TODO: fill these out
-// }
-
-export interface FVTX_VertexAttribute
-{
-    name: string;
-    format: GfxFormat;
-    bufferOffset: number;
-    bufferIndex: number;
-}
-
-export interface FVTX_VertexBuffer
-{
-    stride: number;
-    data: ArrayBufferSlice;
-}
-
-export interface FVTX
-{
-    vertexAttributes: FVTX_VertexAttribute[];
-    vertexBuffers: FVTX_VertexBuffer[];
-    vertexCount: number;
-}
-
 // reads from a bfres file and returns an array of FVTX objects
 // buffer: the bfres file
 // offset: start of the fvtx array
@@ -63,13 +32,11 @@ export function parseFVTX(buffer: ArrayBufferSlice, offset: number, count: numbe
         {
             const stride_offset = buffer_stride_array_offset + (i * 0x10);
             const stride = view.getUint32(stride_offset, true);
-
             const size_offset = buffer_size_array_offset + (i * 0x10);
             const size = view.getUint32(size_offset, true);
-
             const data = buffer.subarray(start_of_buffer, size);
+            
             start_of_buffer = align(start_of_buffer + size, 8);
-
             vertexBuffers.push({ stride, data });
         }
 
@@ -82,71 +49,12 @@ export function parseFVTX(buffer: ArrayBufferSlice, offset: number, count: numbe
         {
             const attribute_name_offset = view.getUint32(attribute_entry_offset, true);
             const name = read_bfres_string(buffer, attribute_name_offset, true);
-            const format = view.getUint32(attribute_entry_offset + 0x8, true);
+            const original_format = view.getUint32(attribute_entry_offset + 0x8, true);
+            const format =convert_attribute_format(original_format);
             const bufferOffset = view.getUint16(attribute_entry_offset + 0xC, true);
             const bufferIndex = view.getUint16(attribute_entry_offset + 0xE, true);
 
-            // convert format to a proper GfxFormat
-            // mk8 convertVertexAttribute and translateAttributeFormat
-
-            let gfx_format = -1;
-
-            switch (format)
-            {
-                case AttributeFormat._8_8_Unorm:
-                    gfx_format = GfxFormat.U8_RG_NORM;
-                    break;
-
-                case AttributeFormat._8_8_Snorm:
-                    gfx_format = GfxFormat.S8_RG_NORM;
-                    break;
-
-                case AttributeFormat._8_8_Uint:
-                    gfx_format = GfxFormat.U8_RG;
-                    break;
-                    
-                case AttributeFormat._8_8_8_8_Unorm:
-                    gfx_format = GfxFormat.U8_RGBA_NORM;
-                    break;
-
-                case AttributeFormat._8_8_8_8_Snorm:
-                    gfx_format = GfxFormat.S8_RGBA_NORM;
-                    break;
-                
-                case AttributeFormat._10_10_10_2_Snorm:
-                    // not supported by webgl, so we need to convert this to S16_RGBA_NORM
-                    break;
-
-                case AttributeFormat._16_16_Unorm:
-                    gfx_format = GfxFormat.U16_RG_NORM;
-                    break;
-
-                case AttributeFormat._16_16_Snorm:
-                    gfx_format = GfxFormat.S16_RG_NORM;
-                    break;
-
-                case AttributeFormat._16_16_Float:
-                    gfx_format = GfxFormat.F16_RG;
-                    break;
-
-                case AttributeFormat._16_16_16_16_Float:
-                    gfx_format = GfxFormat.F16_RGBA;
-                    break;
-
-                case AttributeFormat._32_32_Float:
-                    gfx_format = GfxFormat.F32_RG;
-                    break;
-
-                case AttributeFormat._32_32_32_Float:
-                    gfx_format = GfxFormat.F32_RGB;
-                    break;
-
-                default:
-                    console.error(`attribute format ${format} not found`);
-                    throw "whoops";
-            }
-
-            vertexAttributes.push({ name, format: gfx_format, bufferOffset, bufferIndex });
+            vertexAttributes.push({ name, format, bufferOffset, bufferIndex });
             attribute_entry_offset += 0x10;
         }
 
@@ -155,4 +63,75 @@ export function parseFVTX(buffer: ArrayBufferSlice, offset: number, count: numbe
     }
 
     return fvtx_array;
+}
+
+// Convert the format numbers used by FVTX data into a format number that noclip.website understands
+// format: FVTX attribute format number to convert
+function convert_attribute_format(format: AttributeFormat)
+{
+    switch (format)
+    {
+        case AttributeFormat._8_8_Unorm:
+            return GfxFormat.U8_RG_NORM;
+
+        case AttributeFormat._8_8_Snorm:
+            return GfxFormat.S8_RG_NORM;
+
+        case AttributeFormat._8_8_Uint:
+            return GfxFormat.U8_RG;
+            
+        case AttributeFormat._8_8_8_8_Unorm:
+            return GfxFormat.U8_RGBA_NORM;
+
+        case AttributeFormat._8_8_8_8_Snorm:
+            return GfxFormat.S8_RGBA_NORM;
+        
+        case AttributeFormat._10_10_10_2_Snorm:
+            // not supported by webgl, so we need to convert this to S16_RGBA_NORM
+            // mk8 convertVertexAttribute
+            return -1; //temp for now
+
+        case AttributeFormat._16_16_Unorm:
+            return GfxFormat.U16_RG_NORM;
+
+        case AttributeFormat._16_16_Snorm:
+            return GfxFormat.S16_RG_NORM;
+
+        case AttributeFormat._16_16_Float:
+            return GfxFormat.F16_RG;
+
+        case AttributeFormat._16_16_16_16_Float:
+            return GfxFormat.F16_RGBA;
+
+        case AttributeFormat._32_32_Float:
+            return GfxFormat.F32_RG;
+
+        case AttributeFormat._32_32_32_Float:
+            return GfxFormat.F32_RGB;
+
+        default:
+            console.error(`attribute format ${format} not found`);
+            throw "whoops";
+    }
+}
+
+export interface FVTX_VertexAttribute
+{
+    name: string;
+    format: GfxFormat;
+    bufferOffset: number;
+    bufferIndex: number;
+}
+
+export interface FVTX_VertexBuffer
+{
+    stride: number;
+    data: ArrayBufferSlice;
+}
+
+export interface FVTX
+{
+    vertexAttributes: FVTX_VertexAttribute[];
+    vertexBuffers: FVTX_VertexBuffer[];
+    vertexCount: number;
 }
