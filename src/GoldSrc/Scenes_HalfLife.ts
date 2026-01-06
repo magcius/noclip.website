03efcaa01e3bd1405a331c638ebb7e41ce8fb327
Jack Arthur Harrhy <me@jackharrhy.com>
2026-01-05 20:46:33 -0330

    IdTech2: Quake skyboxes, context for isQuake/isHL1
    - A context system inspired by the source renderer, used for figuring
      out if we're Quake/GoldSrc, had a few places we were checking the BSP
      version which felt more fragile than just doing it once and passing
      around that logic
    - Quake's parallax skybox impl. , based from the eyepos not world space,
      seems to match the base game super well!
    - Put the new Quake specific sky things into a file other than
      Render.ts, to indicate they're quake specific
    - Make IS_HL1 and the like IS_GOLDSRC, on my list is adding CS 1.6
      support, which likely won't need any of its own checks since its just
      a mod ontop of HL1, so a more obvious future-proofing

--- src/GoldSrc/Scenes_HalfLife.ts
+++ src/GoldSrc/Scenes_HalfLife.ts
@@ -3,7 +3,7 @@ import { GfxDevice } from "../gfx/platform/GfxPlatform.js";
 import { SceneContext, SceneDesc, SceneGroup } from "../SceneBase.js";
 import { SceneGfx } from "../viewer.js";
 import { BSPFile } from "../Common/IdTech2/BSPFile.js";
-import { BSPRenderer, IdTech2Renderer } from "../Common/IdTech2/Render.js";
+import { BSPRenderer, IdTech2Context, IdTech2Renderer } from "../Common/IdTech2/Render.js";
 import { parseWAD } from "../Common/IdTech2/WAD.js";
 
 const pathBase = `HalfLife`;
@@ -13,10 +13,12 @@ export class HalfLifeSceneDesc implements SceneDesc {
     }
 
     public async createScene(device: GfxDevice, sceneContext: SceneContext): Promise<SceneGfx> {
-        const renderer = new IdTech2Renderer(device);
-
         const bspData = await sceneContext.dataFetcher.fetchData(`${pathBase}/valve/maps/${this.id}.bsp`);
         const bspFile = new BSPFile(bspData);
+
+        const context = new IdTech2Context(bspFile.version);
+        const renderer = new IdTech2Renderer(device, context);
+
         renderer.textureCache.addBSP(bspFile);
 
         await Promise.all(bspFile.getWadList().map(async (v) => {
@@ -24,7 +26,7 @@ export class HalfLifeSceneDesc implements SceneDesc {
             renderer.textureCache.addWAD(wad);
         }));
 
-        const bspRenderer = new BSPRenderer(renderer.renderHelper.renderCache, renderer.textureCache, bspFile);
+        const bspRenderer = new BSPRenderer(context, renderer.renderHelper.renderCache, renderer.textureCache, bspFile);
         renderer.bspRenderers.push(bspRenderer);
 
         return renderer;
