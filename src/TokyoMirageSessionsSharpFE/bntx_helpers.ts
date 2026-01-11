a038aa81cf56d6e813d77a71af595cef5d409625
emilybrooks <emilybrooksemilybrooks@gmail.com>
2026-01-11 13:16:18 -0700

    cleaning up deswizzle code

---
+++ src/TokyoMirageSessionsSharpFE/bntx_helpers.ts
@@ -0,0 +1,55 @@
+import ArrayBufferSlice from '../ArrayBufferSlice.js';
+import * as BNTX from '../fres_nx/bntx.js';
+import { deswizzle, decompress } from "../fres_nx/tegra_texture.js";
+import { translateImageFormat } from '../fres_nx/tegra_texture.js';
+import { GfxDevice, makeTextureDescriptor2D, GfxTexture } from '../gfx/platform/GfxPlatform.js';
+import { getChannelFormat } from '../fres_nx/nngfx_enum.js';
+
+// textures in a bntx file are swizzled, where the texture data is rearranged for performance reasons
+// this is not supported, and if rendered as is, these textures will look garbled
+// so it's necessary to deswizzle the textures to get the original data back
+// the deswizzle function is asynchronous, so this function also handles uploading the textures as well
+// buffer: the bntx file
+// returns a GfxTexture array of the uploaded textures
+export function deswizzle_and_upload_bntx_textures(buffer: ArrayBufferSlice, device: GfxDevice): GfxTexture[]
+{
+    const bntx = BNTX.parse(buffer);
+    const gfx_texture_array: GfxTexture[] = [];
+
+    for (let i = 0; i < bntx.textures.length; i++)
+    {
+        const texture = bntx.textures[i];
+
+        // create gfx textures
+        const new_format = translateImageFormat(texture.imageFormat);
+        const texture_descriptor = makeTextureDescriptor2D(new_format, texture.width, texture.height, texture.mipBuffers.length);
+        const gfx_texture = device.createTexture(texture_descriptor);
+        gfx_texture_array.push(gfx_texture);
+
+        // deswizzle textures before uploading
+        const channelFormat = getChannelFormat(texture.imageFormat);
+
+        for (let i = 0; i < texture.mipBuffers.length; i++)
+        {
+            const mipLevel = i;
+
+            const buffer = texture.mipBuffers[i];
+            const width = Math.max(texture.width >>> mipLevel, 1);
+            const height = Math.max(texture.height >>> mipLevel, 1);
+            const depth = 1;
+            const blockHeightLog2 = texture.blockHeightLog2;
+            deswizzle({ buffer, width, height, channelFormat, blockHeightLog2 })
+            .then
+            (
+                (deswizzled) =>
+                {
+                    const rgbaTexture = decompress({ ...texture, width, height, depth }, deswizzled);
+                    const rgbaPixels = rgbaTexture.pixels;
+                    device.uploadTextureData(gfx_texture, mipLevel, [rgbaPixels]);
+                }
+            );
+        }
+    }
+
+    return gfx_texture_array;
+}
--- src/TokyoMirageSessionsSharpFE/render.ts
+++ src/TokyoMirageSessionsSharpFE/render.ts
@@ -1,26 +1,20 @@
-import { GfxDevice, GfxVertexAttributeDescriptor, GfxVertexBufferDescriptor, GfxInputLayoutBufferDescriptor,
-         GfxVertexBufferFrequency, GfxInputLayout, GfxFormat, GfxProgram, GfxBufferFrequencyHint,
-         GfxBufferUsage, GfxBindingLayoutDescriptor, GfxCullMode, GfxIndexBufferDescriptor, 
-         GfxBuffer, makeTextureDescriptor2D, GfxSamplerDescriptor, GfxSamplerBinding,
-         GfxTexture} from "../gfx/platform/GfxPlatform.js";
-import { SceneGfx, ViewerRenderInput } from "../viewer.js";
-import { FRES, read_bfres_string } from "./bfres/bfres_switch.js";
+import { FRES } from "./bfres/bfres_switch.js";
+import * as BNTX from '../fres_nx/bntx.js';
+import { deswizzle_and_upload_bntx_textures } from "./bntx_helpers.js";
+import { createBufferFromSlice } from "../gfx/helpers/BufferHelpers.js";
 import { FMDL } from "./bfres/fmdl.js";
 import { GfxRenderHelper } from '../gfx/render/GfxRenderHelper.js';
-import { GfxRenderInstList, GfxRenderInstManager } from '../gfx/render/GfxRenderInstManager.js';
-import { createBufferFromData, createBufferFromSlice } from "../gfx/helpers/BufferHelpers.js";
-import { makeBackbufferDescSimple, standardFullClearRenderPassDescriptor } from '../gfx/helpers/RenderGraphHelpers.js';
+import { GfxRenderInstList } from '../gfx/render/GfxRenderInstManager.js';
 import { GfxrAttachmentSlot } from '../gfx/render/GfxRenderGraph.js';
-import { fillColor, fillMatrix4x3, fillMatrix4x4 } from '../gfx/helpers/UniformBufferHelpers.js';
-import { TMSFEProgram } from './shader.js';
-import { mat4, vec3 } from "gl-matrix";
+import { GfxDevice, GfxVertexAttributeDescriptor, GfxVertexBufferDescriptor, GfxInputLayoutBufferDescriptor,
+         GfxVertexBufferFrequency, GfxInputLayout, GfxBufferFrequencyHint, GfxBufferUsage, GfxBindingLayoutDescriptor,
+         GfxCullMode, GfxBuffer, GfxSamplerBinding, GfxTexture} from "../gfx/platform/GfxPlatform.js";
+import { mat4 } from "gl-matrix";
 import { computeModelMatrixSRT } from "../MathHelpers.js";
-import { assert, readString } from "../util.js";
-import ArrayBufferSlice from "../ArrayBufferSlice.js";
-// import { BNTX, parseBNTX } from "./bntx.js";
-import * as BNTX from '../fres_nx/bntx.js';
-import { TypeFormat, getTypeFormat, ImageFormat, getChannelFormat } from "../fres_nx/nngfx_enum.js";
-import { deswizzle, decompress } from "../fres_nx/tegra_texture.js";
+import { makeBackbufferDescSimple, standardFullClearRenderPassDescriptor } from '../gfx/helpers/RenderGraphHelpers.js';
+import { TMSFEProgram } from './shader.js';
+import { fillMatrix4x3, fillMatrix4x4 } from '../gfx/helpers/UniformBufferHelpers.js';
+import { SceneGfx, ViewerRenderInput } from "../viewer.js";
 
 export class TMSFEScene implements SceneGfx
 {
@@ -37,61 +31,9 @@ export class TMSFEScene implements SceneGfx
             console.log(fres.fmdl[0]);
 
             //initialize textures
-            /*
-            const bntx = parseBNTX(fres.embedded_files[0].buffer);
-            for (let i = 0; i < bntx.textures.length; i++)
-            {
-                const texture = bntx.textures[i];
-                const texture_descriptor = makeTextureDescriptor2D(texture.format, texture.width, texture.height, texture.mipmap_buffers.length);
-                const gfx_texture = device.createTexture(texture_descriptor);
-                device.uploadTextureData(gfx_texture, 0, texture.mipmap_buffers);
-                texture.gfx_texture = gfx_texture;
-            }
-            */
-            const gfx_texture_array: GfxTexture[] = [];
             const bntx = BNTX.parse(fres.embedded_files[0].buffer);
-            for (let i = 0; i < bntx.textures.length; i++)
-            {
-                const texture = bntx.textures[i];
-                function translateImageFormat(imageFormat: ImageFormat): GfxFormat
-                {
-                    const typeFormat = getTypeFormat(imageFormat);
-                
-                    switch (typeFormat)
-                    {
-                    case TypeFormat.Unorm:
-                        return GfxFormat.U8_RGBA_NORM;
-                    case TypeFormat.UnormSrgb:
-                        return GfxFormat.U8_RGBA_SRGB;
-                    case TypeFormat.Snorm:
-                        return GfxFormat.S8_RGBA_NORM;
-                    default:
-                        throw "whoops";
-                    }
-                }
-                const new_format = translateImageFormat(texture.imageFormat);
-                const texture_descriptor = makeTextureDescriptor2D(new_format, texture.width, texture.height, texture.mipBuffers.length);
-                const gfx_texture = device.createTexture(texture_descriptor);
-                gfx_texture_array.push(gfx_texture);
-                // deswizzle
-                const channelFormat = getChannelFormat(texture.imageFormat);
-                for (let i = 0; i < texture.mipBuffers.length; i++)
-                {
-                    const mipLevel = i;
-        
-                    const buffer = texture.mipBuffers[i];
-                    const width = Math.max(texture.width >>> mipLevel, 1);
-                    const height = Math.max(texture.height >>> mipLevel, 1);
-                    const depth = 1;
-                    const blockHeightLog2 = texture.blockHeightLog2;
-                    deswizzle({ buffer, width, height, channelFormat, blockHeightLog2 }).then((deswizzled) => {
-                        const rgbaTexture = decompress({ ...texture, width, height, depth }, deswizzled);
-                        const rgbaPixels = rgbaTexture.pixels;
-                        device.uploadTextureData(gfx_texture, mipLevel, [rgbaPixels]);
-                    });
-                }
-
-            }
+            const bntx_buffer = fres.embedded_files[0].buffer;
+            const gfx_texture_array: GfxTexture[] = deswizzle_and_upload_bntx_textures(bntx_buffer, device);
 
             // create all fshp_renderers
             const fmdl = fres.fmdl[0];
@@ -101,19 +43,6 @@ export class TMSFEScene implements SceneGfx
                 const renderer = new fshp_renderer(device, this.renderHelper, fmdl, i, bntx, gfx_texture_array);
                 this.fshp_renderers.push(renderer);
             }
-
-            // const fvtx = fmdl.fvtx[0];
-            // const view = fvtx.vertexBuffers[1].data.createDataView();
-            // console.log(view.getFloat32(0x0, true));
-            // console.log(view.getFloat32(0x4, true));
-            // console.log(view.getInt16(0x8, true));
-            // console.log(view.getInt16(0xA, true));
-            // console.log(view.getInt16(0xC, true));
-            // console.log(view.getInt16(0xE, true));
-            // console.log(view.getUint16(0x10, true));
-            // console.log(view.getUint16(0x12, true));
-
-            // console.log(view.getFloat32(0x14, true));
         }
     }
 
