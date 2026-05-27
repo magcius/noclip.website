2519c871f7a33ed3b557ded0efdd3ace459b5058
Matt P <mattp3259@gmail.com>
2026-05-26 20:49:47 -0400

    remove more comments

--- go/src/RumbleRacing/core/asset/txf/texture.go
+++ go/src/RumbleRacing/core/asset/txf/texture.go
@@ -38,13 +38,11 @@ func (txf *TXF) GetTextures() []Texture {
 }
 
 const (
-	// Texel storage format
-	PSMCT32 = 0 // [RGBA32], directly stored in 4 bytes
-	PSMCT16 = 2 // [RGBA16, RGBA16] across 4 bytes
+	PSMCT32 = 0
+	PSMCT16 = 2
 
-	// Indexed color (CLUT Types)
-	PSMT8 = 19 // 8 bits per index = 0 -> 255 palette indices
-	PSMT4 = 20 // 4 bits per index = 0 -> 16 palette indices
+	PSMT8 = 19
+	PSMT4 = 20
 )
 
 // Please ignore how awful this function is, I'm aware it's bad.
@@ -157,49 +155,22 @@ func extractTexturesFromZTHE(txf *TXF, clutHeader CLHEEntry, zthe ZTHETexture, z
 		data := txf.textureData.RawData[start : start+colorSize]
 		for pxIndex := range int(size) {
 
-			// get the color index
 			var colorIndex uint32
 			switch zthe.TexelStorageFormat {
 			case PSMT8:
-				// panic("Something went wrong")
-				// just a normal byte
 				colorIndex = uint32(data[pxIndex])
 			case PSMT4:
-
-				// 1. Get the 32 bit offset
-				// each 32 bit word holds 8 indices
 				wordOffset := pxIndex / 8
-				// fmt.Println("px ", pxIndex, "at offset", wordOffset, (len(data)))
-				// 2. get the word.
 				wordStart := wordOffset * 4
-				// TODO: swap little for big change order?
 				word := binary.LittleEndian.Uint32(data[wordStart : wordStart+4])
 
-				// pixel index is index % 8 bits?
 				wordIndex := pxIndex % 8
 				shift := uint(wordIndex * 4)
 				lookup := (word >> shift) & 0xF
-				// fmt.Println("px ", pxIndex, "at word ", wordOffset, len(data), word, wordStart)
 				colorIndex = lookup
-
-				// half the pxIndex will get you the byte base
-				// base := pxIndex / 2
-				// twoColors := uint32(data[base])
-
-				// low := (twoColors & 0xF0) >> 4
-				// high := twoColors & 0xF
-				// // TODO: might need to swap logic here
-				// if (pxIndex % 2) != 0 {
-				// 	colorIndex = low
-				// } else {
-				// 	colorIndex = high
-				// }
 			}
 
 			idx := colorIndex
-
-			// px := binary.LittleEndian.Uint16(swizzledPalette[idx : idx+2])
-			// fmt.Println(zthe.TexelStorageFormat, size)
 			finalPixel := swizzled[idx]
 
 			var R uint8
@@ -209,12 +180,8 @@ func extractTexturesFromZTHE(txf *TXF, clutHeader CLHEEntry, zthe ZTHETexture, z
 
 			switch clutHeader.PixelFormat {
 			case PSMCT16:
-				R, G, B, A = extract16bitRGBA(finalPixel) // 255uint8(a1 * 255)
+				R, G, B, A = extract16bitRGBA(finalPixel)
 			case PSMCT32:
-				// fmt.Println(hex.Dump(paletteDataUnswizzled))
-				// fmt.Println(swizzled)
-				// fmt.Println("final pixel:", finalPixel.Bytes, "idx/colorIdx", idx, colorIndex)
-				// fmt.Println(len(txf.clutHeader.Entries))
 				R, G, B, A = extract32bitRGBA(finalPixel)
 			default:
 				panic("Something went wrong!")
@@ -226,7 +193,6 @@ func extractTexturesFromZTHE(txf *TXF, clutHeader CLHEEntry, zthe ZTHETexture, z
 			img.Set(x, y, color.RGBA{R, G, B, A})
 		}
 
-		// fmt.Println("LOOKUP:", clutHeader.CLUTImageSizeLookup)
 		mipMaps = append(mipMaps, TextureFile{
 			Height:   height,
 			Width:    width,
@@ -236,7 +202,6 @@ func extractTexturesFromZTHE(txf *TXF, clutHeader CLHEEntry, zthe ZTHETexture, z
 	}
 
 	textures = append(textures, Texture{
-		// Name:  fmt.Sprintf("id_%d_%d_%d_%d", zthe.Something, ztheIndex, textureIndex, clhe_index),
 		Name:      fmt.Sprintf("texture_%d", zthe.TextureId),
 		TextureId: zthe.TextureId,
 		Files:     mipMaps,
@@ -249,22 +214,10 @@ func extract32bitRGBA(finalPixel helpers.PixelBytes) (uint8, uint8, uint8, uint8
 	if len(finalPixel.Bytes) != 4 {
 		panic("You messed up.")
 	}
-	// TODO: might need to swap this?
-	// word := binary.LittleEndian.Uint32(finalPixel.Bytes)
-
 	R := finalPixel.Bytes[0]
 	G := finalPixel.Bytes[1]
 	B := finalPixel.Bytes[2]
 	A := uint8(float32(finalPixel.Bytes[3]) / 128.0 * 255.0)
-
-	// A := 255 //finalPixel.Bytes[3]
-	// A := (word & 0xFF000000) >> (8 * 3)
-	// B := (word & 0x00FF0000) >> (8 * 2)
-	// G := (word & 0x0000FF00) >> (8 * 1)
-	// R := (word & 0x000000FF)
-
-	// fmt.Println(len(finalPixel.Bytes), hex.Dump(finalPixel.Bytes), word)
-	// panic("unimplemented")
 
 	return uint8(R), uint8(G), uint8(B), uint8(A)
 }
@@ -275,11 +228,9 @@ func extract16bitRGBA(finalPixel helpers.PixelBytes) (uint8, uint8, uint8, uint8
 	}
 	px := binary.LittleEndian.Uint16(finalPixel.Bytes)
 
-	// Extract 5:5:5 bits
 	r5 := px & 0x1F
 	g5 := (px >> 5) & 0x1F
 	b5 := (px >> 10) & 0x1F
-	// a1 := (px >> 15) & 0x1
 
 	R := uint8((r5 * 255) / 31)
 	G := uint8((g5 * 255) / 31)
