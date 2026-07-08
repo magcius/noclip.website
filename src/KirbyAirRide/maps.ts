19d143df6232d34f44e98ffba8886b7a44a1dfd2
rjd1922 <120694098+rjd1922@users.noreply.github.com>
2026-07-07 23:55:06 -0500

    Update maps.ts
    Per Discord discussion, the map labeled "VS. King Dedede" is actually Destruction Derby 3.

--- src/KirbyAirRide/maps.ts
+++ src/KirbyAirRide/maps.ts
@@ -262,11 +262,10 @@ export const maps = [
 
     new KirbyMapDesc("GrColosseum1", "Destruction Derby 1"),
     new KirbyMapDesc("GrColosseum3", "Destruction Derby 2"),
+    new KirbyMapDesc("GrDedede1", "Destruction Derby 3"),
 
     new KirbyMapDesc("GrPasture1", "Kirby Melee 1"),
     new KirbyMapDesc("GrColosseum5", "Kirby Melee 2"),
-
-    new KirbyMapDesc("GrDedede1", "VS. King Dedede"),
 
     "Test",
     new KirbyMapDesc("GrTest6", "Test6Model"),
